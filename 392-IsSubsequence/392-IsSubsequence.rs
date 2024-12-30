51081e8056b662da257fce01d42994a81b5a1593
Vitaliy Sapronenko <vsapronenko@gmail.com>
2024-12-30 10:58:48 +0200

    Changes inspired by cargo clippy

--- 392-IsSubsequence/392-IsSubsequence.rs
+++ 392-IsSubsequence/392-IsSubsequence.rs
@@ -1,15 +1,12 @@
 impl Solution {
     pub fn is_subsequence(s: String, t: String) -> bool {
         let s = s.as_bytes();
-        let mut t = t.as_bytes().into_iter();
-        let mut position = 0;
+        let mut t = t.as_bytes().iter();
         for letter in s {
-            position = if let Some(position) = t.position(|c| c == letter) {
-                position
-            } else {
+            if !t.any(|c| c == letter) {
                 return false;
             }
         }
-        return true;
+        true
     }
 }
