8ccdd45216dc33449b744003d860beb7c5a4d21b
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-01-02 08:12:02 +0200

    Two pointers lookup is realy faster than binary search for every posible pairs

--- 15-3Sum/15-3Sum.rs
+++ 15-3Sum/15-3Sum.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 impl Solution {
     pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
         let mut nums = nums;
@@ -7,12 +9,20 @@ impl Solution {
             if i > 0 && nums[i] == nums[i - 1] {
                 continue;
             }
-            for j in i + 1..nums.len() - 1 {
-                if nums[j] == nums[j - 1] && j > i + 1 {
-                    continue;
-                }
-                if let Ok(k) = nums[j + 1..].binary_search(&(-nums[i] - nums[j])) {
-                    answer.push(vec![nums[i], nums[j], nums[k + j + 1]]);
+            let mut j = i + 1;
+            let mut k = nums.len() - 1;
+            while j < k {
+                let sum = nums[i] + nums[j] + nums[k];
+                match sum.cmp(&0) {
+                    Ordering::Equal => {
+                        answer.push(vec![nums[i], nums[j], nums[k]]);
+                        j += 1;
+                        while j < nums.len() && nums[j] == nums[j - 1] {
+                            j += 1;
+                        }
+                    }
+                    Ordering::Greater => k -= 1,
+                    Ordering::Less => j += 1,
                 }
             }
         }
