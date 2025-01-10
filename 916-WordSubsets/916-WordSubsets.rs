9cab428f5bafee2cb73064e42ebf414f11ced6e1
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-01-10 08:22:59 +0200

    Update file 10.01.2025, 08:22:59

--- 916-WordSubsets/916-WordSubsets.rs
+++ 916-WordSubsets/916-WordSubsets.rs
@@ -1,32 +1,31 @@
-use std::collections::HashMap;
-
 const ABC_LEN: usize = (b'z' - b'a' + 1) as usize;
 
 #[inline]
-fn to_statistic(word: &str) -> HashMap<u8, i32> {
-    let mut result = HashMap::with_capacity(ABC_LEN);
+fn to_statistic(word: &str) -> Vec<i32> {
+    let mut result = Vec::with_capacity(ABC_LEN);
     for &c in word.as_bytes() {
-        *result.entry(c).or_default() += 1;
+        let c = c as usize;
+        if c >= result.len() {
+            result.resize(c + 1, 0);
+        }
+        *unsafe { result.get_unchecked_mut(c) } += 1;
     }
     result
 }
 
 #[inline]
-fn merge(into: &mut HashMap<u8, i32>, from: HashMap<u8, i32>) {
-    for (c, count) in from {
-        into.entry(c)
-            .and_modify(|value| {
-                if *value < count {
-                    *value = count
-                }
-            })
-            .or_insert(count);
+fn merge(into: &mut Vec<i32>, from: Vec<i32>) {
+    if into.len() < from.len() {
+        into.resize(from.len(), 0);
+    }
+    for (into, from) in into.iter_mut().zip(from.into_iter()) {
+        *into = (*into).max(from);
     }
 }
 
 impl Solution {
     pub fn word_subsets(words1: Vec<String>, words2: Vec<String>) -> Vec<String> {
-        let mut words2_statistic = HashMap::with_capacity(ABC_LEN);
+        let mut words2_statistic = Vec::with_capacity(ABC_LEN);
         for word2 in words2 {
             merge(&mut words2_statistic, to_statistic(&word2));
         }
@@ -34,8 +33,11 @@ impl Solution {
             .into_iter()
             .filter(|word1| {
                 let word1_statistic = to_statistic(word1);
-                for (key, count) in &words2_statistic {
-                    if word1_statistic.get(key).unwrap_or(&0) < count {
+                if word1_statistic.len() < words2_statistic.len() {
+                    return false;
+                }
+                for (count1, &count2) in word1_statistic.into_iter().zip(words2_statistic.iter()) {
+                    if count1 < count2 {
                         return false;
                     }
                 }
