ed7bfff1fe4e34d004161a1a2e12b44ccbf1c737
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-01-15 06:05:06 +0200

    Create file

---
+++ 242-ValidAnagram/242-ValidAnagram.rs
@@ -0,0 +1,18 @@
+const ABC_LEN: usize = (b'z' - b'a' + 1) as usize;
+
+#[inline]
+fn get_statistic(s: String) -> [i32; ABC_LEN] {
+    let mut statistic = [0; ABC_LEN];
+    for &c in s.as_bytes() {
+        statistic[(c - b'a') as usize] += 1;
+    }
+    statistic
+}
+
+impl Solution {
+    pub fn is_anagram(s: String, t: String) -> bool {
+        let s_statistic = get_statistic(s);
+        let t_statistic = get_statistic(t);
+        s_statistic == t_statistic
+    }
+}
