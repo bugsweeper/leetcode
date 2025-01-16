77c1c6511c7ff9d8ad44cb0907c7b226b0b8b6d1
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-01-17 00:21:42 +0200

    Update file 17.01.2025, 00:21:41

--- 49-GroupAnagrams/49-GroupAnagrams.rs
+++ 49-GroupAnagrams/49-GroupAnagrams.rs
@@ -4,14 +4,14 @@ const ABC_LEN: usize = (b'z' - b'a' + 1) as usize;
 fn get_statistic(s: &str) -> [i32; ABC_LEN] {
     let mut statistic = [0; ABC_LEN];
     for &c in s.as_bytes() {
-        statistic[(c - b'a') as usize] += 1;
+        *unsafe{statistic.get_unchecked_mut((c - b'a') as usize)} += 1;
     }
     statistic
 }
 
 impl Solution {
     pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
-        let mut groups: std::collections::HashMap<[i32; 26], Vec<_>> = std::collections::HashMap::new();
+        let mut groups: std::collections::HashMap<[i32; 26], Vec<_>> = std::collections::HashMap::with_capacity(strs.len());
         for item in strs {
             let key = get_statistic(&item);
             groups.entry(key).or_default().push(item);
