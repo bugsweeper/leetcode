42d2d27107c201b2b0df5cc45d41026cb3bc684d
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-01-17 00:17:53 +0200

    Create file

---
+++ 49-GroupAnagrams/49-GroupAnagrams.rs
@@ -0,0 +1,21 @@
+const ABC_LEN: usize = (b'z' - b'a' + 1) as usize;
+
+#[inline]
+fn get_statistic(s: &str) -> [i32; ABC_LEN] {
+    let mut statistic = [0; ABC_LEN];
+    for &c in s.as_bytes() {
+        statistic[(c - b'a') as usize] += 1;
+    }
+    statistic
+}
+
+impl Solution {
+    pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
+        let mut groups: std::collections::HashMap<[i32; 26], Vec<_>> = std::collections::HashMap::new();
+        for item in strs {
+            let key = get_statistic(&item);
+            groups.entry(key).or_default().push(item);
+        }
+        groups.into_values().collect::<Vec<_>>()
+    }
+}
