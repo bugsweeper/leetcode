7eda4fed7422650349a4808b9838230153e17065
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-01-18 23:34:11 +0200

    Update file 18.01.2025, 23:34:11

--- 1368-MinimumCosttoMakeatLeastOneValidPathinaGrid/1368-MinimumCosttoMakeatLeastOneValidPathinaGrid.rs
+++ 1368-MinimumCosttoMakeatLeastOneValidPathinaGrid/1368-MinimumCosttoMakeatLeastOneValidPathinaGrid.rs
@@ -27,12 +27,12 @@ impl Solution {
     pub fn min_cost(grid: Vec<Vec<i32>>) -> i32 {
         let mut grid = grid;
         let m = grid.len();
-        let mut priority_queue = std::collections::BinaryHeap::new();
-        priority_queue.push(Position{i: 0, j: 0, cost: 0});
+        let mut two_side_queue = std::collections::VecDeque::new();
+        two_side_queue.push_front(Position{i: 0, j: 0, cost: 0});
         let n = unsafe{grid.get_unchecked_mut(0)}.len();
         let mut costs = vec![vec![i32::MAX;n];m];
         loop {
-            let Position{i, j, cost} = priority_queue.pop().unwrap();
+            let Position{i, j, cost} = two_side_queue.pop_front().unwrap();
             if i == m - 1 && j == n - 1 {
                 return cost;
             }
@@ -44,38 +44,66 @@ impl Solution {
             // up?
             if i > 0 {
                 let previous_expected_cost = unsafe{costs.get_unchecked_mut(i - 1).get_unchecked_mut(j)};
-                let expected_cost = cost + if free_move == GO_UP {0} else {1};
-                if *previous_expected_cost > expected_cost {
-                    priority_queue.push(Position{i: i - 1, j, cost: expected_cost});
-                    *previous_expected_cost = expected_cost;
+                if free_move == GO_UP {
+                    if *previous_expected_cost > cost {
+                        two_side_queue.push_front(Position{i: i - 1, j, cost});
+                        *previous_expected_cost = cost;
+                    }
+                } else {
+                    let expected_cost = cost + 1;
+                    if *previous_expected_cost > expected_cost {
+                        two_side_queue.push_back(Position{i: i - 1, j, cost: expected_cost});
+                        *previous_expected_cost = expected_cost;
+                    }
                 }
             }
             // down?
             if i < m - 1 {
                 let previous_expected_cost = unsafe{costs.get_unchecked_mut(i + 1).get_unchecked_mut(j)};
-                let expected_cost = cost + if free_move == GO_DOWN {0} else {1};
-                if *previous_expected_cost > expected_cost {
-                    priority_queue.push(Position{i: i + 1, j, cost: expected_cost});
-                    *previous_expected_cost = expected_cost;
+                if free_move == GO_DOWN {
+                    if *previous_expected_cost > cost {
+                        two_side_queue.push_front(Position{i: i + 1, j, cost});
+                        *previous_expected_cost = cost;
+                    }
+                } else {
+                    let expected_cost = cost + 1;
+                    if *previous_expected_cost > expected_cost {
+                        two_side_queue.push_back(Position{i: i + 1, j, cost: expected_cost});
+                        *previous_expected_cost = expected_cost;
+                    }
                 }
             }
             let row = unsafe{costs.get_unchecked_mut(i)};
             // left?
             if j > 0 {
                 let previous_expected_cost = unsafe{row.get_unchecked_mut(j - 1)};
-                let expected_cost = cost + if free_move == GO_LEFT {0} else {1};
-                if *previous_expected_cost > expected_cost {
-                    priority_queue.push(Position{i, j: j - 1, cost: expected_cost});
-                    *previous_expected_cost = expected_cost;
+                if free_move == GO_LEFT {
+                    if *previous_expected_cost > cost {
+                        two_side_queue.push_front(Position{i, j: j - 1, cost});
+                        *previous_expected_cost = cost;
+                    }
+                } else {
+                    let expected_cost = cost + 1;
+                    if *previous_expected_cost > expected_cost {
+                        two_side_queue.push_back(Position{i, j: j - 1, cost: expected_cost});
+                        *previous_expected_cost = expected_cost;
+                    }
                 }
             }
             // right?
             if j < n - 1 {
                 let previous_expected_cost = unsafe{row.get_unchecked_mut(j + 1)};
-                let expected_cost = cost + if free_move == GO_RIGHT {0} else {1};
-                if *previous_expected_cost > expected_cost {
-                    priority_queue.push(Position{i, j: j + 1, cost: expected_cost});
-                    *previous_expected_cost = expected_cost;
+                if free_move == GO_RIGHT {
+                    if *previous_expected_cost > cost {
+                        two_side_queue.push_front(Position{i, j: j + 1, cost});
+                        *previous_expected_cost = cost;
+                    }
+                } else {
+                    let expected_cost = cost + 1;
+                    if *previous_expected_cost > expected_cost {
+                        two_side_queue.push_back(Position{i, j: j + 1, cost: expected_cost});
+                        *previous_expected_cost = expected_cost;
+                    }
                 }
             }
             // mark as visited
