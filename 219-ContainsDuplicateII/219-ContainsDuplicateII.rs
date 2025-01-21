de494f8c36e92a656c4a6c13033ddd242ed6e0e0
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-01-21 10:10:51 +0200

    Update file 21.01.2025, 10:10:50

--- 219-ContainsDuplicateII/219-ContainsDuplicateII.rs
+++ 219-ContainsDuplicateII/219-ContainsDuplicateII.rs
@@ -1,18 +1,13 @@
 impl Solution {
     pub fn contains_nearby_duplicate(nums: Vec<i32>, k: i32) -> bool {
         let k = k as usize;
-        let mut seen = std::collections::HashSet::with_capacity(k);
-        let mut iter = nums.iter();
-        for &item in (&mut iter).take(k) {
-            if !seen.insert(item) {
-                return true;
+        let mut seen = std::collections::HashMap::with_capacity(nums.len());
+        for (index, item) in nums.into_iter().enumerate() {
+            if let Some(prev_index) = seen.insert(item, index) {
+                if index - prev_index <= k {
+                    return true;
+                }
             }
-        }
-        for (old_item, &new_item) in nums.iter().zip(iter) {
-            if !seen.insert(new_item) {
-                return true;
-            }
-            seen.remove(old_item);
         }
         false
     }
