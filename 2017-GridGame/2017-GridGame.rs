da0cb8d7783661ae3eb5dfaccf197e0452203237
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-01-21 09:26:23 +0200

    Update file 21.01.2025, 09:26:23

--- 2017-GridGame/2017-GridGame.rs
+++ 2017-GridGame/2017-GridGame.rs
@@ -11,7 +11,7 @@ fn make_prefix(row: &Vec<i32>) -> (Vec<i64>, i64) {
 impl Solution {
     pub fn grid_game(grid: Vec<Vec<i32>>) -> i64 {
         let (prefix0, sum0) = make_prefix(unsafe { grid.get_unchecked(0) });
-        let (prefix1, sum1) = make_prefix(unsafe { grid.get_unchecked(1) });
+        let (prefix1, _) = make_prefix(unsafe { grid.get_unchecked(1) });
         let (mut iter0, iter1) = (prefix0.into_iter(), prefix1.into_iter());
         let item0 = iter0.next().unwrap();
         let mut max_second = sum0 - item0;
