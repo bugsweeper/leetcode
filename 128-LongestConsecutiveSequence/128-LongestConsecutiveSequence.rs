147c9fdc0a1dcc4ea196c27686a8e5814e900ec8
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-01-22 14:32:26 +0200

    Update file 22.01.2025, 14:32:26

--- 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.rs
+++ 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.rs
@@ -1,40 +1,25 @@
-use std::cmp::Ordering;
 
 impl Solution {
     pub fn longest_consecutive(nums: Vec<i32>) -> i32 {
-        let mut max_length = 0;
-        let mut start2length = std::collections::BTreeMap::new();
-        for num in nums {
-            let mut range = start2length.range(num - max_length..=num + 1);
-            let (insert_num, insert_len, remove) =
-                if let Some((&start, &length)) = range.next_back() {
-                    if start > num {
-                        if let Some((&previous_start, &previous_length)) = range.next_back() {
-                            if previous_start + previous_length == num {
-                                (previous_start, previous_length + 1 + length, Some(start))
-                            } else {
-                                (num, 1 + length, Some(start))
-                            }
-                        } else {
-                            (num, 1 + length, Some(start))
-                        }
-                    } else {
-                        let end: i32 = start + length;
-                        match end.cmp(&num) {
-                            Ordering::Equal => (start, length + 1, None),
-                            Ordering::Greater => continue,
-                            Ordering::Less => (num, 1, None),
-                        }
-                    }
-                } else {
-                    (num, 1, None)
-                };
-            max_length = max_length.max(insert_len);
-            start2length.insert(insert_num, insert_len);
-            if let Some(remove) = remove {
-                start2length.remove(&remove);
-            }
+        if nums.len() == 0 {
+            return 0;
         }
-        max_length
+        let mut nums = nums;
+        nums.sort_unstable();
+        let mut previous = *unsafe { nums.get_unchecked(0) };
+        let mut cur_len = 1;
+        let mut max_len = 1;
+        for num in nums {
+            match num - previous {
+                0 => continue,
+                1 => {
+                    cur_len += 1;
+                    max_len = max_len.max(cur_len);
+                }
+                _ => cur_len = 1,
+            }
+            previous = num;
+        }
+        max_len
     }
 }
