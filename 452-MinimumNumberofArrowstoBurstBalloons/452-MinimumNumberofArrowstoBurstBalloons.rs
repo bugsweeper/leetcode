8553a7c1ca83bef95ae2ea8e6374ec6bbd4547f4
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-01-31 15:52:02 +0200

    Update file 31.01.2025, 15:52:01

--- 452-MinimumNumberofArrowstoBurstBalloons/452-MinimumNumberofArrowstoBurstBalloons.rs
+++ 452-MinimumNumberofArrowstoBurstBalloons/452-MinimumNumberofArrowstoBurstBalloons.rs
@@ -1,13 +1,14 @@
 impl Solution {
     pub fn find_min_arrow_shots(points: Vec<Vec<i32>>) -> i32 {
-        let mut points = points;
+        let mut points = points
+            .into_iter()
+            .map(|vector| unsafe { (*vector.get_unchecked(0), *vector.get_unchecked(1)) })
+            .collect::<Vec<_>>();
         points.sort_unstable();
         let mut arrows = 1;
-        let mut current_end = points[0][1];
-        for balloon in points {
-            let [start, end] = balloon[..] else {
-                continue;
-            };
+        let mut current_end = points[0].1;
+        for balloon in points.into_iter().skip(1) {
+            let (start, end) = balloon;
             if start > current_end {
                 arrows += 1;
                 current_end = end;
