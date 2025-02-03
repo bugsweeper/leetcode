f2bda594e4026488d218e5ffa6f4eb5851e8785b
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-02-03 16:48:30 +0200

    Create file

---
+++ 66-PlusOne/66-PlusOne.rs
@@ -0,0 +1,20 @@
+impl Solution {
+    pub fn plus_one(digits: Vec<i32>) -> Vec<i32> {
+        let mut digits = digits;
+        let mut should_insert_1 = true;
+        for digit in digits.iter_mut().rev() {
+            match *digit {
+                9 => *digit = 0,
+                _ => {
+                    *digit += 1;
+                    should_insert_1 = false;
+                    break;
+                }
+            }
+        }
+        if should_insert_1 {
+            digits.insert(0, 1);
+        }
+        digits
+    }
+}
