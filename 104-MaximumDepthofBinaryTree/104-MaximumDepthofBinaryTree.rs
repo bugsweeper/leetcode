d0518f314afcc15579842920e4157f633b0c6bb2
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-02-05 14:10:41 +0200

    Create file

---
+++ 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.rs
@@ -0,0 +1,29 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        let Some(root) = root else {
+            return 0;
+        };
+        let root = Rc::into_inner(root).unwrap().into_inner();
+        Solution::max_depth(root.left).max(Solution::max_depth(root.right)) + 1
+    }
+}
