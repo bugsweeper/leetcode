7eb3aedabb4d0bf8941acd7e0eb9392bf80ce9b5
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-02-05 15:09:02 +0200

    Create file

---
+++ 108-ConvertSortedArraytoBinarySearchTree/108-ConvertSortedArraytoBinarySearchTree.rs
@@ -0,0 +1,49 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+
+fn sorted_slice_to_bst(nums: &[i32]) -> Option<Rc<RefCell<TreeNode>>> {
+    if nums.is_empty() {
+        None
+    } else {
+        let middle = nums.len() / 2;
+        let left = if middle == 0 {
+            None
+        } else {
+            sorted_slice_to_bst(&nums[0..middle])
+        };
+        let right = if middle == nums.len() - 1 {
+            None
+        } else {
+            sorted_slice_to_bst(&nums[middle + 1..])
+        };
+        Some(Rc::new(RefCell::new(TreeNode {
+            val: nums[middle],
+            left,
+            right,
+        })))
+    }
+}
+
+impl Solution {
+    pub fn sorted_array_to_bst(nums: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
+        sorted_slice_to_bst(&nums[..])
+    }
+}
