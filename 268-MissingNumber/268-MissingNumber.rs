393621937b02e685022ed12610f59cc71eb30e16
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-03-01 15:46:05 +0200

    Sum of vector elements is faster than conversion to HashSet

--- 268-MissingNumber/268-MissingNumber.rs
+++ 268-MissingNumber/268-MissingNumber.rs
@@ -1,11 +1,6 @@
 impl Solution {
     pub fn missing_number(nums: Vec<i32>) -> i32 {
-        let nums: std::collections::HashSet<_> = nums.into_iter().collect();
-        for num in 0..nums.len() as i32 {
-            if !nums.contains(&num) {
-                return num;
-            }
-        }
-        nums.len() as i32
+        let expected_sum = nums.len() * (nums.len() + 1) / 2;
+        expected_sum as i32 - nums.into_iter().sum::<i32>()
     }
 }
