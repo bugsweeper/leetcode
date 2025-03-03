74b95672571e179a58ca650cedb19bdd25ca3a76
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-03-03 08:33:45 +0200

    Partitioning array to separate vectors is much faster than sorting by Ordering state

--- 2161-PartitionArrayAccordingtoGivenPivot/2161-PartitionArrayAccordingtoGivenPivot.rs
+++ 2161-PartitionArrayAccordingtoGivenPivot/2161-PartitionArrayAccordingtoGivenPivot.rs
@@ -1,7 +1,23 @@
+use std::cmp::Ordering;
+
 impl Solution {
     pub fn pivot_array(nums: Vec<i32>, pivot: i32) -> Vec<i32> {
         let mut nums = nums;
-        nums.sort_by_key(|num| num.cmp(&pivot));
+        let mut less = Vec::with_capacity(nums.len());
+        let mut greater = Vec::with_capacity(nums.len());
+        for &num in &nums {
+            match num.cmp(&pivot) {
+                Ordering::Less => less.push(num),
+                Ordering::Equal => {}
+                Ordering::Greater => greater.push(num),
+            }
+        }
+        nums[..less.len()].copy_from_slice(&less[..]);
+        let greater_start = nums.len() - greater.len();
+        for num in &mut nums[less.len()..greater_start] {
+            *num = pivot
+        }
+        nums[greater_start..].copy_from_slice(&greater[..]);
         nums
     }
 }
