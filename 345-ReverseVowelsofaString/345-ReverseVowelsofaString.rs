757c108d4fe99415bc2e053f109aa95fb9f1d368
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-03-04 10:52:18 +0200

    Create file

---
+++ 345-ReverseVowelsofaString/345-ReverseVowelsofaString.rs
@@ -0,0 +1,22 @@
+const VOWELS: [u8; 10] = [b'A', b'E', b'I', b'O', b'U', b'a', b'e', b'i', b'o', b'u'];
+
+impl Solution {
+    pub fn reverse_vowels(s: String) -> String {
+        let vowels = std::collections::HashSet::from(VOWELS);
+        let mut s = s;
+        let s_bytes = unsafe { s.as_bytes_mut() };
+        let mut vowels_position = Vec::with_capacity(s_bytes.len());
+        for (index, character) in s_bytes.iter().enumerate() {
+            if vowels.contains(character) {
+                vowels_position.push(index);
+            }
+        }
+        if vowels_position.len() > 1 {
+            let end = vowels_position.len() - 1;
+            for i in 0..=end / 2 {
+                s_bytes.swap(vowels_position[i], vowels_position[end - i]);
+            }
+        }
+        s
+    }
+}
