050d14657f2fbaa4ef3ad6cf671d06d63a321b7b
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-03-07 12:28:03 +0200

    File updated at 07.03.2025, 12:28:03

--- 230-KthSmallestElementinaBST/230-KthSmallestElementinaBST.rs
+++ 230-KthSmallestElementinaBST/230-KthSmallestElementinaBST.rs
@@ -16,30 +16,52 @@
 //     }
 //   }
 // }
-
 use std::rc::Rc;
 use std::cell::RefCell;
 
-// Returns Err(<smallest value>) or Ok(<element count>)
-// I know it is reversed logic, but it has very easy early return
-fn search(node: &Option<Rc<RefCell<TreeNode>>>, k: i32) -> Result<i32, i32> {
-    if node.is_none() {
-        return Ok(0);
-    }
-    let node_ref = node.as_ref().unwrap().borrow();
-    let left_size = search(&node_ref.left, k)?;
-    if left_size == k - 1 {
-        return Err(node_ref.val);
-    }
-    let right_size = search(&node_ref.right, k - left_size - 1)?;
-    Ok(left_size + right_size + 1)
+#[derive(Copy, Clone)]
+enum KthSmallestResult {
+    Found(i32),
+    NumNodes(i32),
 }
 
 impl Solution {
     pub fn kth_smallest(root: Option<Rc<RefCell<TreeNode>>>, k: i32) -> i32 {
-        if let Err(answer) = search(&root, k) {
-            return answer;
+        match Self::kth_smallest_impl(&root, k - 1) {
+            KthSmallestResult::Found(val) => val,
+            KthSmallestResult::NumNodes(_) => unreachable!(),
         }
-        unimplemented!()
+    }
+
+    fn kth_smallest_impl(root: &Option<Rc<RefCell<TreeNode>>>, mut k: i32) -> KthSmallestResult {
+        let Some(root) = root else {
+            return KthSmallestResult::NumNodes(0);
+        };
+
+        let mut total_nodes = 0;
+
+        let root = root.borrow();
+        match Self::kth_smallest_impl(&root.left, k) {
+            KthSmallestResult::Found(val) => return KthSmallestResult::Found(val),
+            KthSmallestResult::NumNodes(num_nodes) => {
+                k -= num_nodes;
+                total_nodes += num_nodes;
+            },
+        }
+
+        if k == 0 {
+            return KthSmallestResult::Found(root.val);
+        }
+        k -= 1;
+        total_nodes += 1;
+
+        match Self::kth_smallest_impl(&root.right, k) {
+            KthSmallestResult::Found(val) => return KthSmallestResult::Found(val),
+            KthSmallestResult::NumNodes(num_nodes) => {
+                total_nodes += num_nodes;
+            },
+        }
+        
+        KthSmallestResult::NumNodes(total_nodes)
     }
 }
