32343665ce8d371e272140c44a6737ccddff2705
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-03-09 13:53:09 +0200

    File updated at 09.03.2025, 13:53:09

--- 448-FindAllNumbersDisappearedinanArray/448-FindAllNumbersDisappearedinanArray.rs
+++ 448-FindAllNumbersDisappearedinanArray/448-FindAllNumbersDisappearedinanArray.rs
@@ -1,13 +1,16 @@
 impl Solution {
     pub fn find_disappeared_numbers(nums: Vec<i32>) -> Vec<i32> {
-        let mut seen = vec![false; nums.len() + 1];
-        for num in nums {
-            seen[num as usize] = true;
+        let mut vec = Vec::new();
+        vec.resize( nums.len(), 0 );
+        for el in nums {
+            vec[(el as usize)-1] += 1;
         }
-        seen.into_iter()
-            .enumerate()
-            .skip(1)
-            .filter_map(|(index, seen)| if seen { None } else { Some(index as i32) })
-            .collect()
+        let mut ret = Vec::new();
+        for i in 0..vec.len(){
+            if ( vec[i] == 0 ) {
+                ret.push( (i + 1) as i32 );
+            }
+        }
+        return ret;
     }
 }
