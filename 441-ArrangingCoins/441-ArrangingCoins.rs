36076e44abb5ee59229c1667f930420d5e3c3518
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-03-09 13:46:03 +0200

    Create file

---
+++ 441-ArrangingCoins/441-ArrangingCoins.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn arrange_coins(n: i32) -> i32 {
+        let n = n as u64;
+        let mut base = 1;
+        let mut size = n;
+        while size > 1 {
+            let half = size / 2;
+            let middle = base + half;
+            if middle * (middle + 1) / 2 <= n {
+                base = middle;
+            }
+            size -= half;
+        }
+        base as i32
+    }
+}
