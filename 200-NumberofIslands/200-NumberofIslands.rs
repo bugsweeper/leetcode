f8373cf0700d2ca97e72b66977493068760166f2
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-03-11 09:45:52 +0200

    File updated at 11.03.2025, 09:45:52

--- 200-NumberofIslands/200-NumberofIslands.rs
+++ 200-NumberofIslands/200-NumberofIslands.rs
@@ -1,18 +1,16 @@
 const MAX_GRID_SIZE: usize = 300;
 
-use std::collections::VecDeque;
-
 #[inline]
-fn process_grid(i: usize, j: usize, grid: &mut Vec<Vec<char>>, queue: &mut VecDeque<(usize, usize)>) {
-    process_row(i, j, grid.get_mut(i).unwrap(), queue);
+fn process_grid(i: usize, j: usize, grid: &mut Vec<Vec<char>>, stack: &mut Vec<(usize, usize)>) {
+    process_row(i, j, grid.get_mut(i).unwrap(), stack);
 }
 
 #[inline]
-fn process_row(i: usize, j: usize, row: &mut Vec<char>, queue: &mut VecDeque<(usize, usize)>) {
+fn process_row(i: usize, j: usize, row: &mut Vec<char>, stack: &mut Vec<(usize, usize)>) {
     let next = row.get_mut(j).unwrap();
     if *next == '1' {
         *next = '0';
-        queue.push_back((i, j));
+        stack.push((i, j));
     }
 }
 
@@ -22,27 +20,27 @@ impl Solution {
         let m = grid.len();
         let n = grid[0].len();
         let mut result = 0;
-        let mut queue = VecDeque::with_capacity(MAX_GRID_SIZE * MAX_GRID_SIZE);
+        let mut stack = Vec::with_capacity(MAX_GRID_SIZE * MAX_GRID_SIZE);
         for i in 0..m {
             for j in 0..n {
                 let current = &mut grid[i][j];
                 if *current == '1' {
                     result += 1;
                     *current = '0';
-                    queue.push_back((i, j));
-                    while let Some((i, j)) = queue.pop_front() {
+                    stack.push((i, j));
+                    while let Some((i, j)) = stack.pop() {
                         if i > 0 {
-                            process_grid(i - 1, j, &mut grid, &mut queue);
+                            process_grid(i - 1, j, &mut grid, &mut stack);
                         }
                         if i < m - 1 {
-                            process_grid(i + 1, j, &mut grid, &mut queue);
+                            process_grid(i + 1, j, &mut grid, &mut stack);
                         }
                         let mut row = grid.get_mut(i).unwrap();
                         if j > 0 {
-                            process_row(i, j - 1, &mut row, &mut queue);
+                            process_row(i, j - 1, &mut row, &mut stack);
                         }
                         if j < n - 1 {
-                            process_row(i, j + 1, &mut row, &mut queue);
+                            process_row(i, j + 1, &mut row, &mut stack);
                         }
                     }
                 }
