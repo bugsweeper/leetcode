b1f1f62fb7eea8071b07a02eab901dd25e5315b0
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-04-21 09:39:17 +0300

    Create file

---
+++ 120-Triangle/120-Triangle.rs
@@ -0,0 +1,17 @@
+// Last updated: 21.04.2025, 09:39:17
+impl Solution {
+    pub fn minimum_total(triangle: Vec<Vec<i32>>) -> i32 {
+        let mut min_sum_row = vec![0; triangle.len()];
+        for row in triangle {
+            if row.len() > 1 {
+                min_sum_row[row.len() - 1] = min_sum_row[row.len() - 2] + *row.last().unwrap();
+            }
+            let first = *row.first().unwrap();
+            for (i, cell) in row.into_iter().enumerate().skip(1).rev().skip(1) {
+                min_sum_row[i] = min_sum_row[i].min(min_sum_row[i - 1]) + cell;
+            }
+            min_sum_row[0] += first;
+        }
+        min_sum_row.into_iter().min().unwrap()
+    }
+}
