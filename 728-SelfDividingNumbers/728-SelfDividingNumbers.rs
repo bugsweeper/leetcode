c84d3dd171b76f1c0a7680aa7e186c798de4d31e
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-04-24 13:11:31 +0300

    Create file

---
+++ 728-SelfDividingNumbers/728-SelfDividingNumbers.rs
@@ -0,0 +1,25 @@
+// Last updated: 24.04.2025, 13:11:31
+#[inline]
+fn is_self_dividing(number: i32) -> bool {
+    let mut remaining = number;
+    while remaining > 0 {
+        let digit = remaining % 10;
+        if digit == 0 || number % digit != 0 {
+            return false;
+        }
+        remaining /= 10;
+    }
+    true
+}
+
+impl Solution {
+    pub fn self_dividing_numbers(left: i32, right: i32) -> Vec<i32> {
+        let mut array = Vec::with_capacity(10000);
+        for i in left..=right {
+            if is_self_dividing(i) {
+                array.push(i);
+            }
+        }
+        array
+    }
+}
