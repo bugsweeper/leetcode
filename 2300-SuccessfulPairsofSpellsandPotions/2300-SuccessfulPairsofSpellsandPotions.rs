40c8a7ec9fb088d8fb6bdeac3525824eec89c21a
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-05-09 23:07:20 +0300

    Create file

---
+++ 2300-SuccessfulPairsofSpellsandPotions/2300-SuccessfulPairsofSpellsandPotions.rs
@@ -0,0 +1,17 @@
+// Last updated: 09.05.2025, 23:07:20
+impl Solution {
+    pub fn successful_pairs(spells: Vec<i32>, potions: Vec<i32>, success: i64) -> Vec<i32> {
+        let mut potions = potions;
+        potions.sort_unstable();
+        let success = success as u64;
+        spells
+            .into_iter()
+            .map(|spell| {
+                let successful_potion = success.div_ceil(spell as u64);
+                let first_successful_potion_index =
+                    potions.partition_point(|&potion| (potion as u64) < successful_potion);
+                (potions.len() - first_successful_potion_index) as i32
+            })
+            .collect()
+    }
+}
