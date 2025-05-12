79f2c49cbd9c3afc763ea208ae93d97d4dfa1695
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-05-12 23:13:14 +0300

    File updated at 12.05.2025, 23:13:14

--- 933-NumberofRecentCalls/933-NumberofRecentCalls.rs
+++ 933-NumberofRecentCalls/933-NumberofRecentCalls.rs
@@ -1,35 +1,41 @@
-// Last updated: 12.05.2025, 22:33:17
-use std::collections::VecDeque;
+// Last updated: 12.05.2025, 23:13:14
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
 
-struct RecentCounter {
-    requests: VecDeque<i32>
+pub fn range_sum_bst(node: &Option<Rc<RefCell<TreeNode>>>, low: i32, high: i32) -> i32 {
+    if node.is_none() {
+        return 0;
+    }
+    let node_ref = node.as_ref().unwrap().borrow();
+    if node_ref.val < low {
+        return range_sum_bst(&node_ref.right, low, high);
+    }
+    if node_ref.val > high {
+        return range_sum_bst(&node_ref.left, low, high);
+    }
+    range_sum_bst(&node_ref.left, low, high) + node_ref.val + range_sum_bst(&node_ref.right, low, high)
 }
 
-
-/** 
- * `&self` means the method takes an immutable reference.
- * If you need a mutable reference, change it to `&mut self` instead.
- */
-impl RecentCounter {
-
-    fn new() -> Self {
-        Self {
-            requests: VecDeque::with_capacity(10_000)
-        }
-    }
-    
-    fn ping(&mut self, t: i32) -> i32 {
-        self.requests.push_back(t);
-        let lower_limit = t - 3000;
-        while *self.requests.front().unwrap() < t - 3000 {
-            self.requests.pop_front();
-        }
-        self.requests.len() as i32
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn range_sum_bst(root: Option<Rc<RefCell<TreeNode>>>, low: i32, high: i32) -> i32 {
+        range_sum_bst(&root, low, high)
     }
 }
-
-/**
- * Your RecentCounter object will be instantiated and called as such:
- * let obj = RecentCounter::new();
- * let ret_1: i32 = obj.ping(t);
- */
