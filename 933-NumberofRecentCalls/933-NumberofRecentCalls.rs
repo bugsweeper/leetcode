3e5659e2a774ee322dc602f87a220ff60d0e009e
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-05-12 23:50:49 +0300

    File updated at 12.05.2025, 23:50:49

--- 933-NumberofRecentCalls/933-NumberofRecentCalls.rs
+++ 933-NumberofRecentCalls/933-NumberofRecentCalls.rs
@@ -1,41 +1,30 @@
-// Last updated: 12.05.2025, 23:13:14
-// Definition for a binary tree node.
-// #[derive(Debug, PartialEq, Eq)]
-// pub struct TreeNode {
-//   pub val: i32,
-//   pub left: Option<Rc<RefCell<TreeNode>>>,
-//   pub right: Option<Rc<RefCell<TreeNode>>>,
-// }
-// 
-// impl TreeNode {
-//   #[inline]
-//   pub fn new(val: i32) -> Self {
-//     TreeNode {
-//       val,
-//       left: None,
-//       right: None
-//     }
-//   }
-// }
+// Last updated: 12.05.2025, 23:50:49
+use std::cmp::Ordering;
 
-pub fn range_sum_bst(node: &Option<Rc<RefCell<TreeNode>>>, low: i32, high: i32) -> i32 {
-    if node.is_none() {
-        return 0;
-    }
-    let node_ref = node.as_ref().unwrap().borrow();
-    if node_ref.val < low {
-        return range_sum_bst(&node_ref.right, low, high);
-    }
-    if node_ref.val > high {
-        return range_sum_bst(&node_ref.left, low, high);
-    }
-    range_sum_bst(&node_ref.left, low, high) + node_ref.val + range_sum_bst(&node_ref.right, low, high)
-}
-
-use std::rc::Rc;
-use std::cell::RefCell;
 impl Solution {
-    pub fn range_sum_bst(root: Option<Rc<RefCell<TreeNode>>>, low: i32, high: i32) -> i32 {
-        range_sum_bst(&root, low, high)
+    pub fn valid_mountain_array(arr: Vec<i32>) -> bool {
+        let mut prev = arr[0];
+        let mut middle = 0;
+        for (index, height) in arr.iter().skip(1).enumerate() {
+            match prev.cmp(height) {
+                Ordering::Greater => {
+                    middle = index;
+                    break;
+                }
+                Ordering::Equal => return false,
+                _ => {}
+            }
+            prev = *height;
+        }
+        if middle == 0 || middle == arr.len() - 1 {
+            return false;
+        }
+        for &height in &arr[middle + 1..] {
+            if height >= prev {
+                return false;
+            }
+            prev = height
+        }
+        true
     }
 }
