250fc33cbf80a65821b55c77b7ab4f5161bb8fe0
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-05-14 14:58:04 +0300

    File updated at 14.05.2025, 14:58:04

--- 933-NumberofRecentCalls/933-NumberofRecentCalls.rs
+++ 933-NumberofRecentCalls/933-NumberofRecentCalls.rs
@@ -1,19 +1,29 @@
-// Last updated: 13.05.2025, 16:58:26
+// Last updated: 14.05.2025, 14:58:04
+use std::cmp::Ordering;
+
+const ABC_LEN: usize = (b'z' - b'a' + 1) as usize;
+
 impl Solution {
-    pub fn min_deletion_size(strs: Vec<String>) -> i32 {
-        let str_len = strs[0].len();
-        let mut deleted = 0;
-        for j in 0..str_len {
-            let mut prev = strs[0].as_bytes()[j];
-            for row in strs.iter().skip(1) {
-                let cur = row.as_bytes()[j];
-                if cur < prev {
-                    deleted += 1;
-                    break;
+    pub fn is_alien_sorted(words: Vec<String>, order: String) -> bool {
+        let mut indexes = vec![0; ABC_LEN];
+        for (index, &c) in order.as_bytes().iter().enumerate() {
+            indexes[(c - b'a') as usize] = index;
+        }
+        'words: for slice in words.windows(2) {
+            let [first, second] = slice else {
+                unimplemented!()
+            };
+            for (first, second) in first.as_bytes().iter().zip(second.as_bytes()) {
+                match indexes[(first - b'a') as usize].cmp(&indexes[(second - b'a') as usize]) {
+                    Ordering::Greater => return false,
+                    Ordering::Less => continue 'words,
+                    _ => {}
                 }
-                prev = cur;
+            }
+            if first.len() > second.len() {
+                return false;
             }
         }
-        deleted
+        true
     }
 }
