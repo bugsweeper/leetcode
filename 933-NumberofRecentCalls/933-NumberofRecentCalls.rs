2f72d1a0b7e4159379da6e3d9a54dadf47b09d86
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-05-14 21:08:09 +0300

    File updated at 14.05.2025, 21:08:09

--- 933-NumberofRecentCalls/933-NumberofRecentCalls.rs
+++ 933-NumberofRecentCalls/933-NumberofRecentCalls.rs
@@ -1,36 +1,16 @@
-// Last updated: 14.05.2025, 16:46:55
-// Definition for a binary tree node.
-// #[derive(Debug, PartialEq, Eq)]
-// pub struct TreeNode {
-//   pub val: i32,
-//   pub left: Option<Rc<RefCell<TreeNode>>>,
-//   pub right: Option<Rc<RefCell<TreeNode>>>,
-// }
-// 
-// impl TreeNode {
-//   #[inline]
-//   pub fn new(val: i32) -> Self {
-//     TreeNode {
-//       val,
-//       left: None,
-//       right: None
-//     }
-//   }
-// }
-use std::rc::Rc;
-use std::cell::RefCell;
-
-fn is_same(node: &Option<Rc<RefCell<TreeNode>>>, value: i32) -> bool {
-    if node.is_none() {
-        return true;
-    }
-    let node_ref = node.as_ref().unwrap().borrow();
-    node_ref.val == value && is_same(&node_ref.left, value) && is_same(&node_ref.right, value)
-}
-
+// Last updated: 14.05.2025, 21:08:09
 impl Solution {
-    pub fn is_unival_tree(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
-        let root_ref = root.as_ref().unwrap().borrow();
-        is_same(&root_ref.left, root_ref.val) && is_same(&root_ref.right, root_ref.val)
+    pub fn largest_perimeter(nums: Vec<i32>) -> i32 {
+        let mut nums = nums;
+        nums.sort_unstable_by_key(|num| -num);
+        for slice in nums.windows(3) {
+            let &[a, b, c] = slice else {
+                unimplemented!();
+            };
+            if a < b + c {
+                return a + b + c;
+            }
+        }
+        0
     }
 }
