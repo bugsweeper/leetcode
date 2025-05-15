eb72842ccc46faab38897ffadf0dc1884efce5e5
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-05-15 15:23:29 +0300

    File updated at 15.05.2025, 15:23:29

--- 933-NumberofRecentCalls/933-NumberofRecentCalls.rs
+++ 933-NumberofRecentCalls/933-NumberofRecentCalls.rs
@@ -1,57 +1,25 @@
-// Last updated: 15.05.2025, 14:54:42
+// Last updated: 15.05.2025, 15:23:29
+const ABC_LEN: usize = (b'z' - b'a' + 1) as usize;
+
 impl Solution {
-    pub fn num_rook_captures(board: Vec<Vec<char>>) -> i32 {
-        let (mut rook_i, mut rook_j) = (0, 0);
-        'board: for (i, row) in board.iter().enumerate() {
-            for (j, &cell) in row.iter().enumerate() {
-                if cell == 'R' {
-                    (rook_i, rook_j) = (i, j);
-                    break 'board;
-                }
+    pub fn common_chars(words: Vec<String>) -> Vec<String> {
+        let mut min = [usize::MAX; ABC_LEN];
+        for word in words {
+            let mut frequency = [0; ABC_LEN];
+            for &byte in word.as_bytes() {
+                frequency[(byte - b'a') as usize] += 1;
+            }
+            for (min, frequency) in min.iter_mut().zip(frequency) {
+                *min = (*min).min(frequency);
             }
         }
-        let mut pawns = 0;
-        for i in rook_i + 1..8 {
-            match board[i][rook_j] {
-                'p' => {
-                    pawns += 1;
-                    break;
-                }
-                'B' => break,
-                _ => {},
+        let mut result = Vec::with_capacity(100);
+        for (index, min) in min.into_iter().enumerate() {
+            let character = (index as u8 + b'a') as char;
+            for _ in 0..min {
+                result.push(character.into());
             }
         }
-        for i in (0..rook_i).rev() {
-            match board[i][rook_j] {
-                'p' => {
-                    pawns += 1;
-                    break;
-                }
-                'B' => break,
-                _ => {},
-            }
-        }
-        let row = &board[rook_i];
-        for j in rook_j + 1..8 {
-            match row[j] {
-                'p' => {
-                    pawns += 1;
-                    break;
-                }
-                'B' => break,
-                _ => {},
-            }
-        }
-        for j in (0..rook_j).rev() {
-            match row[j] {
-                'p' => {
-                    pawns += 1;
-                    break;
-                }
-                'B' => break,
-                _ => {},
-            }
-        }
-        pawns
+        result
     }
 }
