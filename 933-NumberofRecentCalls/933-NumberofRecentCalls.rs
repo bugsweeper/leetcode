4b56be122dbfa887eac477d11c706992826d4384
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-05-15 14:18:53 +0300

    File updated at 15.05.2025, 14:18:52

--- 933-NumberofRecentCalls/933-NumberofRecentCalls.rs
+++ 933-NumberofRecentCalls/933-NumberofRecentCalls.rs
@@ -1,38 +1,21 @@
-// Last updated: 15.05.2025, 13:43:23
-// Definition for a binary tree node.
-// #[derive(Debug, PartialEq, Eq)]
-// pub struct TreeNode {
-//   pub val: i32,
-//   pub left: Option<Rc<RefCell<TreeNode>>>,
-//   pub right: Option<Rc<RefCell<TreeNode>>>,
-// }
-// 
-// impl TreeNode {
-//   #[inline]
-//   pub fn new(val: i32) -> Self {
-//     TreeNode {
-//       val,
-//       left: None,
-//       right: None
-//     }
-//   }
-// }
-use std::rc::Rc;
-use std::cell::RefCell;
-
-// returns depth and parent
-fn search(node: &Option<Rc<RefCell<TreeNode>>>, depth: usize, parent_value: i32, value: i32) -> Option<(usize, i32)> {
-    let node_ref = node.as_ref()?.borrow();
-    if node_ref.val == value {
-        return Some((depth, parent_value));
-    }
-    search(&node_ref.left, depth + 1, node_ref.val, value).or(search(&node_ref.right, depth + 1, node_ref.val, value))
-}
-
+// Last updated: 15.05.2025, 14:18:52
 impl Solution {
-    pub fn is_cousins(root: Option<Rc<RefCell<TreeNode>>>, x: i32, y: i32) -> bool {
-        let (x_depth, x_parent) = search(&root, 0, 0, x).unwrap();
-        let (y_depth, y_parent) = search(&root, 0, 0, y).unwrap();
-        x_depth == y_depth && x_parent != y_parent
+    pub fn find_judge(n: i32, trust: Vec<Vec<i32>>) -> i32 {
+        let n = n as usize;
+        let mut trusts = vec![false; n + 1];
+        let mut trusted = vec![0; n + 1];
+        for relation in trust {
+            let [a, b] = relation[..] else {
+                unimplemented!()
+            };
+            trusts[a as usize] = true;
+            trusted[b as usize] += 1; 
+        }
+        for (label, (trusts, trusted)) in trusts.into_iter().zip(trusted).enumerate().skip(1) {
+            if !trusts && trusted == n - 1 {
+                return label as i32;
+            }
+        }
+        -1
     }
 }
