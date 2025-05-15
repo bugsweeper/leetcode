4c1e6ec7feff3ebccc228c741540c310d0c397c4
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-05-15 14:54:42 +0300

    File updated at 15.05.2025, 14:54:42

--- 933-NumberofRecentCalls/933-NumberofRecentCalls.rs
+++ 933-NumberofRecentCalls/933-NumberofRecentCalls.rs
@@ -1,21 +1,57 @@
-// Last updated: 15.05.2025, 14:18:52
+// Last updated: 15.05.2025, 14:54:42
 impl Solution {
-    pub fn find_judge(n: i32, trust: Vec<Vec<i32>>) -> i32 {
-        let n = n as usize;
-        let mut trusts = vec![false; n + 1];
-        let mut trusted = vec![0; n + 1];
-        for relation in trust {
-            let [a, b] = relation[..] else {
-                unimplemented!()
-            };
-            trusts[a as usize] = true;
-            trusted[b as usize] += 1; 
-        }
-        for (label, (trusts, trusted)) in trusts.into_iter().zip(trusted).enumerate().skip(1) {
-            if !trusts && trusted == n - 1 {
-                return label as i32;
+    pub fn num_rook_captures(board: Vec<Vec<char>>) -> i32 {
+        let (mut rook_i, mut rook_j) = (0, 0);
+        'board: for (i, row) in board.iter().enumerate() {
+            for (j, &cell) in row.iter().enumerate() {
+                if cell == 'R' {
+                    (rook_i, rook_j) = (i, j);
+                    break 'board;
+                }
             }
         }
-        -1
+        let mut pawns = 0;
+        for i in rook_i + 1..8 {
+            match board[i][rook_j] {
+                'p' => {
+                    pawns += 1;
+                    break;
+                }
+                'B' => break,
+                _ => {},
+            }
+        }
+        for i in (0..rook_i).rev() {
+            match board[i][rook_j] {
+                'p' => {
+                    pawns += 1;
+                    break;
+                }
+                'B' => break,
+                _ => {},
+            }
+        }
+        let row = &board[rook_i];
+        for j in rook_j + 1..8 {
+            match row[j] {
+                'p' => {
+                    pawns += 1;
+                    break;
+                }
+                'B' => break,
+                _ => {},
+            }
+        }
+        for j in (0..rook_j).rev() {
+            match row[j] {
+                'p' => {
+                    pawns += 1;
+                    break;
+                }
+                'B' => break,
+                _ => {},
+            }
+        }
+        pawns
     }
 }
