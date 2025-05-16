311e5b8f23c22652078c585a7240ae1d3f61beeb
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-05-16 14:03:10 +0300

    Create file

---
+++ 611-ValidTriangleNumber/611-ValidTriangleNumber.rs
@@ -0,0 +1,14 @@
+// Last updated: 16.05.2025, 14:03:10
+impl Solution {
+    pub fn triangle_number(nums: Vec<i32>) -> i32 {
+        let mut nums = nums;
+        nums.sort_unstable();
+        let mut count = 0;
+        for (index1, num1) in nums.iter().enumerate().take(nums.len() - 2) {
+            for (index2, num2) in nums.iter().enumerate().skip(index1 + 1).take(nums.len() - index1 - 1) {
+                count += nums[index2 + 1..].partition_point(|&num3| num3 < num1 + num2);
+            }
+        }
+        count as i32
+    }
+}
