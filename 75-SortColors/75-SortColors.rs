3b4b824499efc7f1557036341215d549fc40b086
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-05-17 09:57:10 +0300

    File updated at 17.05.2025, 09:57:10

--- 75-SortColors/75-SortColors.rs
+++ 75-SortColors/75-SortColors.rs
@@ -1,8 +1,8 @@
-// Last updated: 17.05.2025, 09:56:49
+// Last updated: 17.05.2025, 09:57:10
 impl Solution {
     pub fn sort_colors(nums: &mut Vec<i32>) {
         let mut count = [0; 3];
-        for num in nums.iter_mut() {
+        for num in nums.iter() {
             count[*num as usize] += 1;
         }
         let (mut start, mut end) = (0, 0);
