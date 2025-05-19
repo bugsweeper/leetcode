de574c55d12e9247e1609edfea619de70d96d63b
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-05-19 15:53:28 +0300

    File updated at 19.05.2025, 15:53:27

--- 1071-GreatestCommonDivisorofStrings/1071-GreatestCommonDivisorofStrings.rs
+++ 1071-GreatestCommonDivisorofStrings/1071-GreatestCommonDivisorofStrings.rs
@@ -1,20 +1,16 @@
-// Last updated: 19.05.2025, 15:49:02
+// Last updated: 19.05.2025, 15:53:27
 impl Solution {
-    pub fn relative_sort_array(arr1: Vec<i32>, arr2: Vec<i32>) -> Vec<i32> {
-        let mut indexes = vec![usize::MAX; 1001];
-        for (index, &num1) in arr2.iter().enumerate() {
-            indexes[num1 as usize] = index;
-        }
-        let mut arr1 = arr1;
-        arr1.sort_by_key(|num| {
-            let num = *num as usize;
-            let index = indexes[num];
-            if index == usize::MAX {
-                num + 1001
-            } else {
-                index
+    pub fn tribonacci(n: i32) -> i32 {
+        match n {
+            0 => 0,
+            1 | 2 => 1,
+            _ => {
+                let (mut t0, mut t1, mut t2) = (0, 1, 1);
+                for _ in 2..n {
+                    (t0, t1, t2) = (t1, t2, t0 + t1 + t2);
+                }
+                t2
             }
-        });
-        arr1
+        }
     }
 }
