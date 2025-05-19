ce6d04b9f7f6f7d7d1f685fe6fed7d66516e0bb8
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-05-19 11:36:38 +0300

    File updated at 19.05.2025, 11:36:37

--- 1037-ValidBoomerang/1037-ValidBoomerang.rs
+++ 1037-ValidBoomerang/1037-ValidBoomerang.rs
@@ -1,15 +1,22 @@
-// Last updated: 19.05.2025, 10:32:13
+// Last updated: 19.05.2025, 11:36:37
+use std::collections::BinaryHeap;
+
 impl Solution {
-    pub fn is_boomerang(points: Vec<Vec<i32>>) -> bool {
-        let [ref a, ref b, ref c] = points[..] else {
-            unimplemented!();
-        };
-        let (&[ax, ay], &[bx, by], &[cx, cy]) = (&a[..], &b[..], &c[..]) else {
-            unimplemented!()
-        };
-        let (abx, aby) = (bx - ax, by - ay);
-        let (acx, acy) = (cx - ax, cy - ay);
-        let vector_product_height = abx * acy - acx * aby;
-        vector_product_height != 0
+    pub fn last_stone_weight(stones: Vec<i32>) -> i32 {
+        let mut heap = stones.into_iter().collect::<BinaryHeap<_>>();
+        loop {
+            if let Some(y) = heap.pop() {
+                if let Some(x) = heap.pop() {
+                    if y > x {
+                        heap.push(y - x);
+                    }
+                } else {
+                    return y;
+                }
+            } else {
+                return 0;
+            }
+        }
+        unimplemented!();
     }
 }
