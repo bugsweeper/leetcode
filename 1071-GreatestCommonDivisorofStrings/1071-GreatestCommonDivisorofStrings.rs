4f7e466dd99ce72ae3987748fc521d4af119542c
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-05-19 13:51:45 +0300

    File updated at 19.05.2025, 13:51:44

--- 1071-GreatestCommonDivisorofStrings/1071-GreatestCommonDivisorofStrings.rs
+++ 1071-GreatestCommonDivisorofStrings/1071-GreatestCommonDivisorofStrings.rs
@@ -1,48 +1,24 @@
-// Last updated: 19.05.2025, 13:18:56
-fn consist_of(mut goal: &str, cd: &str) -> bool {
-    loop {
-        let (head, remaining) = goal.split_at(cd.len());
-        if head != cd {
-            return false;
-        }
-        if remaining.is_empty() {
-            return true;
-        }
-        goal = remaining;
-    }
-}
-
+// Last updated: 19.05.2025, 13:51:44
 impl Solution {
-    pub fn gcd_of_strings(str1: String, str2: String) -> String {
-        let (long, short) = if str1.len() > str2.len() {
-            (str1, str2)
+    pub fn find_ocurrences(text: String, first: String, second: String) -> Vec<String> {
+        let mut iter = text.split_ascii_whitespace();
+        let mut word1 = if let Some(word) = iter.next() {
+            word
         } else {
-            (str2, str1)
+            return Vec::new();
         };
-        if long
-            .as_bytes()
-            .iter()
-            .zip(short.as_bytes())
-            .any(|(byte1, byte2)| byte1 != byte2)
-        {
-            return String::new();
-        }
-        if long.len() == short.len() {
-            return short;
-        }
-        let (mut len, mut gcd_len) = (long.len(), short.len());
-        while len % gcd_len != 0 {
-            (len, gcd_len) = (gcd_len, len % gcd_len);
-        }
-        for cd_len in (1..=gcd_len).rev() {
-            if gcd_len % cd_len != 0 {
-                continue;
+        let mut word2 = if let Some(word) = iter.next() {
+            word
+        } else {
+            return Vec::new();
+        };
+        let mut thirds = Vec::new();
+        for word3 in iter {
+            if word1 == first && word2 == second {
+                thirds.push(word3.into());
             }
-            let cd = &short[..cd_len];
-            if consist_of(&long, cd) {
-                return cd.into();
-            }
+            (word1, word2) = (word2, word3)
         }
-        String::new()
+        thirds
     }
 }
