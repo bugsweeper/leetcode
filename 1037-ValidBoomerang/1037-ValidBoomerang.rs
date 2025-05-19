627bdf26c8202b3319d0305a345791e065fc32bf
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-05-19 12:01:02 +0300

    File updated at 19.05.2025, 12:01:02

--- 1037-ValidBoomerang/1037-ValidBoomerang.rs
+++ 1037-ValidBoomerang/1037-ValidBoomerang.rs
@@ -1,18 +1,12 @@
-// Last updated: 19.05.2025, 11:51:24
+// Last updated: 19.05.2025, 12:01:02
 impl Solution {
-    pub fn remove_duplicates(s: String) -> String {
-        let mut stack = Vec::with_capacity(s.len());
-        for byte in s.as_bytes() {
-            if let Some(last) = stack.last() {
-                if last == byte {
-                    stack.pop();
-                } else {
-                    stack.push(*byte);
-                }
-            } else {
-                stack.push(*byte);
-            }
-        }
-        String::from_utf8(stack).unwrap()
+    pub fn height_checker(heights: Vec<i32>) -> i32 {
+        let mut expected = heights.clone();
+        expected.sort_unstable();
+        expected
+            .into_iter()
+            .zip(heights)
+            .filter(|(expected, height)| expected != height)
+            .count() as i32
     }
 }
