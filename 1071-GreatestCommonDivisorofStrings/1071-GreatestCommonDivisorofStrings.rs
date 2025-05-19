17688a1b89a626ac0f4f5f8548f5a4b041945e23
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-05-19 15:49:02 +0300

    File updated at 19.05.2025, 15:49:02

--- 1071-GreatestCommonDivisorofStrings/1071-GreatestCommonDivisorofStrings.rs
+++ 1071-GreatestCommonDivisorofStrings/1071-GreatestCommonDivisorofStrings.rs
@@ -1,14 +1,20 @@
-// Last updated: 19.05.2025, 15:22:08
+// Last updated: 19.05.2025, 15:49:02
 impl Solution {
-    pub fn defang_i_paddr(address: String) -> String {
-        let mut defanged = String::with_capacity(address.len() + 8);
-        for character in address.chars() {
-            if character == '.' {
-                defanged.push_str("[.]");
-            } else {
-                defanged.push(character);
-            }
+    pub fn relative_sort_array(arr1: Vec<i32>, arr2: Vec<i32>) -> Vec<i32> {
+        let mut indexes = vec![usize::MAX; 1001];
+        for (index, &num1) in arr2.iter().enumerate() {
+            indexes[num1 as usize] = index;
         }
-        defanged
+        let mut arr1 = arr1;
+        arr1.sort_by_key(|num| {
+            let num = *num as usize;
+            let index = indexes[num];
+            if index == usize::MAX {
+                num + 1001
+            } else {
+                index
+            }
+        });
+        arr1
     }
 }
