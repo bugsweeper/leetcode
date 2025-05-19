402ba4a3d914e9dee96d036d7babc17c65e2ae45
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-05-19 15:18:21 +0300

    File updated at 19.05.2025, 15:18:20

--- 1071-GreatestCommonDivisorofStrings/1071-GreatestCommonDivisorofStrings.rs
+++ 1071-GreatestCommonDivisorofStrings/1071-GreatestCommonDivisorofStrings.rs
@@ -1,20 +1,20 @@
-// Last updated: 19.05.2025, 15:08:00
+// Last updated: 19.05.2025, 15:18:20
 impl Solution {
-    pub fn duplicate_zeros(arr: &mut Vec<i32>) {
-        let mut zeros = Vec::with_capacity(arr.len() / 2);
-        for (index, &num) in arr.iter().enumerate() {
-            if num == 0 {
-                if index + zeros.len() >= arr.len() - 1 {
-                    break;
+    pub fn distribute_candies(candies: i32, num_people: i32) -> Vec<i32> {
+        let mut candy = 1;
+        let mut people = vec![0; num_people as usize];
+        let mut candies = candies;
+        while candies > 0 {
+            for human in people.iter_mut() {
+                let give = candy.min(candies);
+                *human += give;
+                candies -= give;
+                if candies == 0 {
+                    return people;
                 }
-                zeros.push(index);
+                candy += 1;
             }
         }
-        let mut end = arr.len() - zeros.len();
-        for (prev_count, zero_index) in zeros.into_iter().enumerate().rev() {
-            arr.copy_within(zero_index..end, zero_index + prev_count + 1);
-            arr[zero_index + prev_count] = 0;
-            end = zero_index;
-        }
+        people
     }
 }
