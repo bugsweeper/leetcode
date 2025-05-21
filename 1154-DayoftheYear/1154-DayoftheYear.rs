0d17aace2824d654b159e2fbfa04afec295f2495
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-05-21 13:05:07 +0300

    File updated at 21.05.2025, 13:05:07

--- 1154-DayoftheYear/1154-DayoftheYear.rs
+++ 1154-DayoftheYear/1154-DayoftheYear.rs
@@ -1,27 +1,24 @@
-// Last updated: 21.05.2025, 12:07:37
-const ABC_LEN: usize = (b'z' - b'a' + 1) as usize;
-
-fn count_characters(word: &str) -> [usize; ABC_LEN] {
-    let mut count = [0; ABC_LEN];
-    for &c in word.as_bytes() {
-        count[(c - b'a') as usize] += 1;
-    }
-    count
-}
+// Last updated: 21.05.2025, 13:05:07
+const MODULO: i64 = 1_000_000_007;
+const PRIMES: [i32; 25] = [
+    2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
+];
+const FACTORIALS: [i64; 76] = [
+    1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600, 227020758,
+    178290591, 674358851, 789741546, 425606191, 660911389, 557316307, 146326063, 72847302,
+    602640637, 860734560, 657629300, 440732388, 459042011, 394134213, 35757887, 36978716,
+    109361473, 390205642, 486580460, 57155068, 943272305, 14530444, 523095984, 354551275,
+    472948359, 444985875, 799434881, 776829897, 626855450, 954784168, 10503098, 472639410,
+    741412713, 846397273, 627068824, 726372166, 318608048, 249010336, 948537388, 272481214,
+    713985458, 269199917, 75195247, 286129051, 595484846, 133605669, 16340084, 996745124,
+    798197261, 286427093, 331333826, 536698543, 422103593, 280940535, 103956247, 172980994,
+    108669496, 715534167, 518459667, 847555432, 719101534, 932614679,
+];
 
 impl Solution {
-    pub fn count_characters(words: Vec<String>, chars: String) -> i32 {
-        let chars_count = count_characters(&chars);
-        let mut result = 0;
-        'word: for word in words {
-            let word_count = count_characters(&word);
-            for (&chars_count, &word_count) in chars_count.iter().zip(word_count.iter()) {
-                if chars_count < word_count {
-                    continue 'word;
-                }
-            }
-            result += word.len() as i32;
-        }
-        result
+    pub fn num_prime_arrangements(n: i32) -> i32 {
+        let primes = PRIMES.partition_point(|&prime| prime <= n);
+        let n = n as usize;
+        (FACTORIALS[primes] * FACTORIALS[n - primes] % MODULO) as i32
     }
 }
