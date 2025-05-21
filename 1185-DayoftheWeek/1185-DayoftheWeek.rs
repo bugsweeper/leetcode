0c99565d588aef1f2771bbcdb1c6c3a960b64361
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-05-21 15:02:18 +0300

    File updated at 21.05.2025, 15:02:18

--- 1185-DayoftheWeek/1185-DayoftheWeek.rs
+++ 1185-DayoftheWeek/1185-DayoftheWeek.rs
@@ -1,38 +1,17 @@
-// Last updated: 21.05.2025, 14:54:25
-use std::cmp::Ordering;
-
+// Last updated: 21.05.2025, 15:02:18
 impl Solution {
-    pub fn minimum_abs_difference(arr: Vec<i32>) -> Vec<Vec<i32>> {
-        let mut arr = arr;
-        arr.sort_unstable();
-        let mut min_difference = i64::MAX;
-        let mut difference_count = 0;
-        for slice in arr.windows(2) {
-            let &[a, b] = slice else {
-                unimplemented!();
-            };
-            let difference = b as i64 - a as i64;
-            match difference.cmp(&min_difference) {
-                Ordering::Less => {
-                    min_difference = difference;
-                    difference_count = 1;
-                }
-                Ordering::Equal => difference_count += 1,
-                _ => {}
+    pub fn unique_occurrences(arr: Vec<i32>) -> bool {
+        let mut counts = vec![0; 2001];
+        for num in arr {
+            counts[(num + 1000) as usize] += 1;
+        }
+        counts.sort_unstable();
+        let non_zero_start = counts.partition_point(|&count| count == 0);
+        for slice in counts[non_zero_start..].windows(2) {
+            if slice[0] == slice[1] {
+                return false;
             }
         }
-        let mut result = Vec::with_capacity(difference_count);
-        result.extend(arr.windows(2).filter_map(|slice| {
-            let &[a, b] = slice else {
-                unimplemented!();
-            };
-            let difference = b as i64 - a as i64;
-            if difference == min_difference {
-                Some(vec![a, b])
-            } else {
-                None
-            }
-        }));
-        result
+        true
     }
 }
