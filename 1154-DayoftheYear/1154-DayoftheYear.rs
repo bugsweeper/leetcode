6d461d53725920966cc3dbe1174a89fe93b981d0
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-05-21 12:07:38 +0300

    File updated at 21.05.2025, 12:07:37

--- 1154-DayoftheYear/1154-DayoftheYear.rs
+++ 1154-DayoftheYear/1154-DayoftheYear.rs
@@ -1,16 +1,27 @@
-// Last updated: 21.05.2025, 11:20:11
-// const array with days preceding before each month
-const MONTHS_PRECEDING_DAYS: [i32; 12] = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
+// Last updated: 21.05.2025, 12:07:37
+const ABC_LEN: usize = (b'z' - b'a' + 1) as usize;
+
+fn count_characters(word: &str) -> [usize; ABC_LEN] {
+    let mut count = [0; ABC_LEN];
+    for &c in word.as_bytes() {
+        count[(c - b'a') as usize] += 1;
+    }
+    count
+}
 
 impl Solution {
-    pub fn day_of_year(date: String) -> i32 {
-        let year = date[..4].parse::<i32>().unwrap();
-        let month = date[5..7].parse::<usize>().unwrap() - 1;
-        let day = date[8..].parse::<i32>().unwrap();
-        let mut day_of_year = day + MONTHS_PRECEDING_DAYS[month];
-        if month > 1 && (year % 400 == 0 || year % 4 == 0 && year % 100 != 0)  {
-            day_of_year += 1;
+    pub fn count_characters(words: Vec<String>, chars: String) -> i32 {
+        let chars_count = count_characters(&chars);
+        let mut result = 0;
+        'word: for word in words {
+            let word_count = count_characters(&word);
+            for (&chars_count, &word_count) in chars_count.iter().zip(word_count.iter()) {
+                if chars_count < word_count {
+                    continue 'word;
+                }
+            }
+            result += word.len() as i32;
         }
-        day_of_year
+        result
     }
 }
