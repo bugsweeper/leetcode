3927b84a832ad9a6d406337f24f0612465a88d30
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-05-21 16:31:53 +0300

    File updated at 21.05.2025, 16:31:52

--- 1185-DayoftheWeek/1185-DayoftheWeek.rs
+++ 1185-DayoftheWeek/1185-DayoftheWeek.rs
@@ -1,18 +1,22 @@
-// Last updated: 21.05.2025, 16:24:11
+// Last updated: 21.05.2025, 16:31:52
 impl Solution {
-    pub fn balanced_string_split(s: String) -> i32 {
-        let mut balance = 0;
-        let mut count = 0;
-        for &byte in s.as_bytes() {
-            if byte == b'L' {
-                balance += 1;
-            } else {
-                balance -= 1;
-            }
-            if balance == 0 {
-                count += 1;
+    pub fn check_straight_line(coordinates: Vec<Vec<i32>>) -> bool {
+        let mut iter = coordinates.into_iter();
+        let [x0, y0] = iter.next().unwrap()[..] else {
+            unimplemented!();
+        };
+        let [x1, y1] = iter.next().unwrap()[..] else {
+            unimplemented!();
+        };
+        let (dx0, dy0) = (x1 - x0, y1 - y0);
+        for slice in iter {
+            let [x, y] = slice[..] else {
+                unimplemented!();
+            };
+            if (x - x0) * dy0 != dx0 * (y - y0) {
+                return false;
             }
         }
-        count
+        true
     }
 }
