865e6f650427d00077fefdf0d0a0537982c265f5
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-05-21 15:05:37 +0300

    File updated at 21.05.2025, 15:05:37

--- 1185-DayoftheWeek/1185-DayoftheWeek.rs
+++ 1185-DayoftheWeek/1185-DayoftheWeek.rs
@@ -1,17 +1,14 @@
-// Last updated: 21.05.2025, 15:02:18
+// Last updated: 21.05.2025, 15:05:37
 impl Solution {
-    pub fn unique_occurrences(arr: Vec<i32>) -> bool {
-        let mut counts = vec![0; 2001];
-        for num in arr {
-            counts[(num + 1000) as usize] += 1;
-        }
-        counts.sort_unstable();
-        let non_zero_start = counts.partition_point(|&count| count == 0);
-        for slice in counts[non_zero_start..].windows(2) {
-            if slice[0] == slice[1] {
-                return false;
+    pub fn min_cost_to_move_chips(position: Vec<i32>) -> i32 {
+        let (mut even, mut odd) = (0, 0);
+        for position in position {
+            if position & 1 == 0 {
+                even += 1;
+            } else {
+                odd += 1;
             }
         }
-        true
+        odd.min(even)
     }
 }
