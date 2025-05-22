69b91fba2fe434286a9b6633a7a23412794d9541
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-05-22 14:45:26 +0300

    File updated at 22.05.2025, 14:45:26

--- 1260-Shift2DGrid/1260-Shift2DGrid.rs
+++ 1260-Shift2DGrid/1260-Shift2DGrid.rs
@@ -1,29 +1,11 @@
-// Last updated: 22.05.2025, 14:40:37
-// Definition for singly-linked list.
-// #[derive(PartialEq, Eq, Clone, Debug)]
-// pub struct ListNode {
-//   pub val: i32,
-//   pub next: Option<Box<ListNode>>
-// }
-// 
-// impl ListNode {
-//   #[inline]
-//   fn new(val: i32) -> Self {
-//     ListNode {
-//       next: None,
-//       val
-//     }
-//   }
-// }
+// Last updated: 22.05.2025, 14:45:26
 impl Solution {
-    pub fn get_decimal_value(head: Option<Box<ListNode>>) -> i32 {
-        let mut node = &head;
-        let mut value = 0;
-        while node.is_some() {
-            let node_ref = node.as_ref().unwrap();
-            value = (value << 1) + node_ref.val;
-            node = &node_ref.next;
+    pub fn replace_elements(arr: Vec<i32>) -> Vec<i32> {
+        let mut arr = arr;
+        let mut max = -1;
+        for num in arr.iter_mut().rev() {
+            (*num, max) = (max, max.max(*num))
         }
-        value
+        arr
     }
 }
