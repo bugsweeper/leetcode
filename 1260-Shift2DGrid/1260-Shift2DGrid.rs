c8641b512e9da7dd1aa63a8b91c8fe854d45b616
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-05-22 15:31:16 +0300

    File updated at 22.05.2025, 15:31:16

--- 1260-Shift2DGrid/1260-Shift2DGrid.rs
+++ 1260-Shift2DGrid/1260-Shift2DGrid.rs
@@ -1,12 +1,15 @@
-// Last updated: 22.05.2025, 14:50:35
+// Last updated: 22.05.2025, 15:31:16
 impl Solution {
-    pub fn sum_zero(n: i32) -> Vec<i32> {
-        let mut arr = Vec::with_capacity(n as usize);
-        arr.extend(-n / 2..0);
-        if n & 1 == 1 {
-            arr.push(0);
+    pub fn freq_alphabets(s: String) -> String {
+        let mut decrypted = Vec::with_capacity(s.len());
+        let mut iter = s.as_bytes().iter().rev();
+        while let Some(&byte) = iter.next() {
+            if byte == b'#' {
+                decrypted.push(*iter.next().unwrap() - b'0' + 10 * (*iter.next().unwrap() - b'1') + b'j');
+            } else {
+                decrypted.push(byte - b'1' + b'a');
+            }
         }
-        arr.extend(1..=n / 2);
-        arr
+        decrypted.into_iter().map(|byte| byte as char).rev().collect::<String>()
     }
 }
