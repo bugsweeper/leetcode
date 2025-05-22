efe7ccca4fd0b250f414c994f24b6bfb32dd2fcc
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-05-22 14:50:35 +0300

    File updated at 22.05.2025, 14:50:35

--- 1260-Shift2DGrid/1260-Shift2DGrid.rs
+++ 1260-Shift2DGrid/1260-Shift2DGrid.rs
@@ -1,11 +1,12 @@
-// Last updated: 22.05.2025, 14:45:26
+// Last updated: 22.05.2025, 14:50:35
 impl Solution {
-    pub fn replace_elements(arr: Vec<i32>) -> Vec<i32> {
-        let mut arr = arr;
-        let mut max = -1;
-        for num in arr.iter_mut().rev() {
-            (*num, max) = (max, max.max(*num))
+    pub fn sum_zero(n: i32) -> Vec<i32> {
+        let mut arr = Vec::with_capacity(n as usize);
+        arr.extend(-n / 2..0);
+        if n & 1 == 1 {
+            arr.push(0);
         }
+        arr.extend(1..=n / 2);
         arr
     }
 }
