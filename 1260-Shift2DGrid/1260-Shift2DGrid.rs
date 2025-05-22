df0c711a21a847c99e3b5d52c55796e7cf06e7bb
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-05-22 14:31:33 +0300

    File updated at 22.05.2025, 14:31:33

--- 1260-Shift2DGrid/1260-Shift2DGrid.rs
+++ 1260-Shift2DGrid/1260-Shift2DGrid.rs
@@ -1,15 +1,16 @@
-// Last updated: 22.05.2025, 14:14:32
+// Last updated: 22.05.2025, 14:31:33
 impl Solution {
-    pub fn subtract_product_and_sum(n: i32) -> i32 {
-        let mut sum = 0;
-        let mut product = 1;
-        let mut n = n;
-        while n > 0 {
-            let digit = n % 10;
-            sum += digit;
-            product *= digit;
-            n /= 10;
+    pub fn find_special_integer(arr: Vec<i32>) -> i32 {
+        let candidates = [*arr.first().unwrap(), arr[arr.len() / 4], arr[arr.len() / 2], arr[3 * arr.len() / 4]];
+        let mut max_count = 0;
+        let mut goal = i32::MAX;
+        for candidate in candidates {
+            let count = arr.partition_point(|&num| num <= candidate) - arr.partition_point(|&num| num < candidate);
+            if count > max_count {
+                goal = candidate;
+                max_count = count;
+            }
         }
-        product - sum
+        goal
     }
 }
