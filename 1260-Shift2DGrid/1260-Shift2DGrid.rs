3e0a4b94b68cb9fe770e4b5512e25da73adb718a
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-05-22 16:56:46 +0300

    File updated at 22.05.2025, 16:56:46

--- 1260-Shift2DGrid/1260-Shift2DGrid.rs
+++ 1260-Shift2DGrid/1260-Shift2DGrid.rs
@@ -1,13 +1,22 @@
-// Last updated: 22.05.2025, 15:38:39
+// Last updated: 22.05.2025, 16:56:46
 impl Solution {
-    pub fn decompress_rl_elist(nums: Vec<i32>) -> Vec<i32> {
-        let mut result = Vec::with_capacity(nums.len() / 2);
-        for slice in nums.chunks(2) {
-            let &[freq, val] = slice else {
-                unimplemented!();
-            };
-            result.extend(std::iter::repeat_n(val, freq as usize));
+    pub fn get_no_zero_integers(n: i32) -> Vec<i32> {
+        let mut n = n - 1;
+        let mut position = 1;
+        let mut pair = 1;
+        while n > position {
+            n -= position;
+            pair += position;
+            if n < position {
+                break;
+            }
+            let digit = n / position % 10;
+            if digit == 0 {
+                n -= position;
+                pair += position;
+            }
+            position *= 10;
         }
-        result
+        vec![n, pair]
     }
 }
