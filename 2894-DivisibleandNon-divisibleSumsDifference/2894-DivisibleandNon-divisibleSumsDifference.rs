f9d706de6c40cdd5284f8e2830ee93a47a05be30
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-05-27 08:12:53 +0300

    File updated at 27.05.2025, 08:12:53

--- 2894-DivisibleandNon-divisibleSumsDifference/2894-DivisibleandNon-divisibleSumsDifference.rs
+++ 2894-DivisibleandNon-divisibleSumsDifference/2894-DivisibleandNon-divisibleSumsDifference.rs
@@ -1,13 +1,7 @@
-// Last updated: 27.05.2025, 08:09:16
+// Last updated: 27.05.2025, 08:12:53
 impl Solution {
     pub fn difference_of_sums(n: i32, m: i32) -> i32 {
-        let (num1, num2) = (1..=n).fold((0, 0), |(num1, num2), num| {
-            if num % m == 0 {
-                (num1, num2 + num)
-            } else {
-                (num1 + num, num2)
-            }
-        });
-        num1 - num2
+        let n2 = n / m;
+        n * (n + 1) / 2 - m * n2 * (n2 + 1)
     }
 }
