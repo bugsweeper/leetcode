88972c81320130018ed29a945fd9a9146879b4bb
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-05-28 16:46:05 +0300

    File updated at 28.05.2025, 16:46:04

--- 1323-Maximum69Number/1323-Maximum69Number.rs
+++ 1323-Maximum69Number/1323-Maximum69Number.rs
@@ -1,14 +1,11 @@
-// Last updated: 28.05.2025, 16:40:36
+// Last updated: 28.05.2025, 16:46:04
 impl Solution {
-    pub fn maximum69_number (num: i32) -> i32 {
-        let mut position = 10_i32.pow(num.ilog10() + 1);
-        while position > 0 {
-            let digit = num / position % 10;
-            if digit == 6 {
-                return num + 3 * position;
-            }
-            position /= 10;
-        }
-        num
+    pub fn array_rank_transform(arr: Vec<i32>) -> Vec<i32> {
+        let mut sorted = arr.clone();
+        sorted.sort_unstable();
+        sorted.dedup();
+        arr.into_iter()
+            .map(|value| sorted.partition_point(|&sorted| sorted < value) as i32 + 1)
+            .collect()
     }
 }
