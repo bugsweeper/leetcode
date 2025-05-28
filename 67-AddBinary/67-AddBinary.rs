e870b1ce04f9107687e872054d932fe7fbf613da
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-05-28 15:57:34 +0300

    File updated at 28.05.2025, 15:57:34

--- 67-AddBinary/67-AddBinary.rs
+++ 67-AddBinary/67-AddBinary.rs
@@ -1,19 +1,17 @@
+// Last updated: 28.05.2025, 15:57:34
 use std::collections::VecDeque;
 
 impl Solution {
     pub fn add_binary(a: String, b: String) -> String {
         let mut buffer = VecDeque::with_capacity(a.len().max(b.len()) + 1);
         let mut next_position_bit = 0;
-        let mut current_bit = b'0';
+        let mut current_bit;
         let min_size = a.len().min(b.len());
         let max_size = a.len().max(b.len());
-        let mut a_iter = a
-        .as_bytes()
-        .into_iter()
-        .rev();
-        let mut b_iter = b.as_bytes().into_iter().rev();
+        let mut a_iter = a.as_bytes().iter().rev();
+        let mut b_iter = b.as_bytes().iter().rev();
         for _ in 0..min_size {
-            let (&a, &b) = unsafe{ (a_iter.next().unwrap_unchecked(), b_iter.next().unwrap_unchecked()) };
+            let (&a, &b) = (a_iter.next().unwrap(), b_iter.next().unwrap());
             (next_position_bit, current_bit) = match (a, b, next_position_bit) {
                 (b'0', b'0', 0) => (0, b'0'),
                 (b'1', b'1', 1) => (1, b'1'),
@@ -22,29 +20,23 @@ impl Solution {
             };
             buffer.push_front(current_bit);
         }
-        let mut single_iter = if a.len() > b.len() {
-            a_iter
-        } else {
-            b_iter
-        };
+        let mut single_iter = if a.len() > b.len() { a_iter } else { b_iter };
         for _ in min_size..max_size {
             if next_position_bit == 0 {
-                while let Some(&a) = single_iter.next() {
+                for &a in single_iter {
                     buffer.push_front(a);
                 }
                 break;
+            } else if *single_iter.next().unwrap() == b'0' {
+                buffer.push_front(b'1');
+                next_position_bit = 0;
             } else {
-                if *unsafe { single_iter.next().unwrap_unchecked() } == b'0' {
-                    buffer.push_front(b'1');
-                    next_position_bit = 0;
-                } else {
-                    buffer.push_front(b'0');
-                }
+                buffer.push_front(b'0');
             }
         }
         if next_position_bit == 1 {
             buffer.push_front(b'1');
         }
-        unsafe { String::from_utf8_unchecked(buffer.into()) }
+        String::from_utf8(buffer.into()).unwrap()
     }
 }
