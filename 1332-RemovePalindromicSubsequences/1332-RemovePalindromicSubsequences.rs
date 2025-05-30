10d9dbbc81dcc1b22ad64ad03ffe8ba2281bf76d
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-05-30 14:26:59 +0300

    File updated at 30.05.2025, 14:26:59

--- 1332-RemovePalindromicSubsequences/1332-RemovePalindromicSubsequences.rs
+++ 1332-RemovePalindromicSubsequences/1332-RemovePalindromicSubsequences.rs
@@ -1,10 +1,8 @@
-// Last updated: 30.05.2025, 14:22:16
+// Last updated: 30.05.2025, 14:26:59
 impl Solution {
-    pub fn number_of_steps(num: i32) -> i32 {
-        if num == 0 {
-            0
-        } else {
-            (num.ilog2() + num.count_ones()) as i32
-        }
+    pub fn sort_by_bits(arr: Vec<i32>) -> Vec<i32> {
+        let mut arr = arr;
+        arr.sort_by_cached_key(|value| (value.count_ones(), *value));
+        arr
     }
 }
