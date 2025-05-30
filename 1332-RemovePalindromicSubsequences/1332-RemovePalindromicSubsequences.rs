3bc037cc72949991c434ca3f16cdd35f3edf27f1
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-05-30 14:22:16 +0300

    File updated at 30.05.2025, 14:22:16

--- 1332-RemovePalindromicSubsequences/1332-RemovePalindromicSubsequences.rs
+++ 1332-RemovePalindromicSubsequences/1332-RemovePalindromicSubsequences.rs
@@ -1,9 +1,10 @@
-// Last updated: 30.05.2025, 14:18:24
-const SOLDIER: i32 = 1;
+// Last updated: 30.05.2025, 14:22:16
 impl Solution {
-    pub fn k_weakest_rows(mat: Vec<Vec<i32>>, k: i32) -> Vec<i32> {
-        let mut mat = mat.into_iter().enumerate().map(|(index, row)| (row.partition_point(|&human| human == SOLDIER), index as i32, row)).collect::<Vec<_>>();
-        mat.sort_by_key(|(soldiers, ..)| *soldiers);
-        mat.into_iter().take(k as usize).map(|(_, index, _)| index).collect::<Vec<_>>()
+    pub fn number_of_steps(num: i32) -> i32 {
+        if num == 0 {
+            0
+        } else {
+            (num.ilog2() + num.count_ones()) as i32
+        }
     }
 }
