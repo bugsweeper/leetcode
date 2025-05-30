bda43a7479309827487d930a2225c3ab0c8a9663
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-05-30 16:29:13 +0300

    File updated at 30.05.2025, 16:29:13

--- 1332-RemovePalindromicSubsequences/1332-RemovePalindromicSubsequences.rs
+++ 1332-RemovePalindromicSubsequences/1332-RemovePalindromicSubsequences.rs
@@ -1,12 +1,27 @@
-// Last updated: 30.05.2025, 16:11:27
+// Last updated: 30.05.2025, 16:29:13
+const ABC_LEN: usize = (b'z' - b'a' + 1) as usize;
+
 impl Solution {
-    pub fn smaller_numbers_than_current(nums: Vec<i32>) -> Vec<i32> {
-        let mut sorted = nums.clone();
-        sorted.sort_unstable();
-        let mut nums = nums;
-        for num in &mut nums {
-            *num = sorted.partition_point(|sorted| sorted < num) as i32;
+    pub fn sort_string(s: String) -> String {
+        let mut count = [0; ABC_LEN];
+        for &byte in s.as_bytes() {
+            count[(byte - b'a') as usize] += 1;
         }
-        nums
+        let mut result = String::with_capacity(s.len());
+        while result.len() < s.len() {
+            for (index, count) in count.iter_mut().enumerate() {
+                if *count > 0 {
+                    result.push((index as u8 + b'a') as char);
+                    *count -= 1;
+                }
+            }
+            for (index, count) in count.iter_mut().enumerate().rev() {
+                if *count > 0 {
+                    result.push((index as u8 + b'a') as char);
+                    *count -= 1;
+                }
+            }
+        }
+        result
     }
 }
