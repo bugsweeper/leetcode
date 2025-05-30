d25724348c9d3bf0b1c1a726c10b5f4548a7bb0e
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-05-30 16:50:36 +0300

    File updated at 30.05.2025, 16:50:36

--- 1332-RemovePalindromicSubsequences/1332-RemovePalindromicSubsequences.rs
+++ 1332-RemovePalindromicSubsequences/1332-RemovePalindromicSubsequences.rs
@@ -1,15 +1,23 @@
-// Last updated: 30.05.2025, 16:36:15
-use std::iter::repeat_n;
-
+// Last updated: 30.05.2025, 16:50:36
 impl Solution {
-    pub fn generate_the_string(n: i32) -> String {
-        let n = n as usize;
-        let mut result = String::with_capacity(n);
-        if n & 1 == 0 {
-            result.extend(repeat_n('a', n - 1));
-            result.push('b');
-        } else {
-            result.extend(repeat_n('a', n));
+    pub fn lucky_numbers(matrix: Vec<Vec<i32>>) -> Vec<i32> {
+        let n = matrix.len();
+        let m = matrix[0].len();
+        let mut columns = vec![i32::MIN; m];
+        let mut rows = Vec::with_capacity(n);
+        for row in &matrix {
+            rows.push(*row.iter().min().unwrap());
+            for (&cell, max_column) in row.iter().zip(columns.iter_mut()) {
+                *max_column = (*max_column).max(cell);
+            }
+        }
+        let mut result = Vec::with_capacity(n.min(m));
+        for (row, min_row) in matrix.iter().zip(rows.into_iter()) {
+            for (&cell, &max_column) in row.iter().zip(columns.iter()) {
+                if cell == min_row && cell == max_column {
+                    result.push(cell);
+                }
+            }
         }
         result
     }
