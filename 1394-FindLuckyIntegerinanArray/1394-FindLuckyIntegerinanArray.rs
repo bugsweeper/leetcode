e8978ccd2db33cde73dfd366d4b78acd39978f2d
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-06-02 12:43:40 +0300

    Create 1394-FindLuckyIntegerinanArray

---
+++ 1394-FindLuckyIntegerinanArray/1394-FindLuckyIntegerinanArray.rs
@@ -0,0 +1,17 @@
+// Last updated: 02.06.2025, 12:43:40
+impl Solution {
+    pub fn find_lucky(arr: Vec<i32>) -> i32 {
+        let mut freq = vec![0; arr.len() + 1];
+        for num in arr {
+            if let Some(freq) = freq.get_mut(num as usize) {
+                *freq += 1;
+            }
+        }
+        for (index, freq) in freq.into_iter().enumerate().skip(1).rev() {
+            if index as i32 == freq {
+                return freq;
+            }
+        }
+        -1
+    }
+}
