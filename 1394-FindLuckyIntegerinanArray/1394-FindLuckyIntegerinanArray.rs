e05c805d357ee37a857ec9c4e0d32c36b9af3941
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-06-02 16:45:21 +0300

    File updated at 02.06.2025, 16:45:21

--- 1394-FindLuckyIntegerinanArray/1394-FindLuckyIntegerinanArray.rs
+++ 1394-FindLuckyIntegerinanArray/1394-FindLuckyIntegerinanArray.rs
@@ -1,35 +1,8 @@
-// Last updated: 02.06.2025, 15:32:35
+// Last updated: 02.06.2025, 16:45:21
 impl Solution {
-    pub fn reformat(s: String) -> String {
-        let mut s = s;
-        let mut digits = Vec::new();
-        let mut letters = Vec::new();
-        for &c in s.as_bytes() {
-            if c.is_ascii_digit() {
-                digits.push(c);
-            } else if c.is_ascii_alphabetic() {
-                letters.push(c);
-            }
-        }
-        if digits.len().abs_diff(letters.len()) > 1 {
-            return String::new();
-        }
-        let mut result = String::with_capacity(s.len());
-        let mut digit_iter = digits.iter();
-        let mut letter_iter = letters.iter();
-        if letters.len() > digits.len() {
-            result.push(*letter_iter.next().unwrap() as char);
-        }
-        loop {
-            if let Some(digit) = digit_iter.next() {
-                result.push(*digit as char);
-            }
-            if let Some(letter) = letter_iter.next() {
-                result.push(*letter as char);
-            } else {
-                break;
-            }
-        }
-        result
+    pub fn kids_with_candies(candies: Vec<i32>, extra_candies: i32) -> Vec<bool> {
+        let max_candies = *candies.iter().max().unwrap();
+        let min_to_have = max_candies - extra_candies;
+        candies.into_iter().map(|candies| candies >= min_to_have).collect()
     }
 }
