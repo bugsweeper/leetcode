942f23ea4032c3dae55edf644801bf9d867d9bdd
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-06-02 12:36:44 +0300

    File updated at 02.06.2025, 12:36:44

--- 1385-FindtheDistanceValueBetweenTwoArrays/1385-FindtheDistanceValueBetweenTwoArrays.rs
+++ 1385-FindtheDistanceValueBetweenTwoArrays/1385-FindtheDistanceValueBetweenTwoArrays.rs
@@ -1,19 +1,10 @@
-// Last updated: 02.06.2025, 12:30:05
+// Last updated: 02.06.2025, 12:36:44
 impl Solution {
-    pub fn find_the_distance_value(arr1: Vec<i32>, arr2: Vec<i32>, d: i32) -> i32 {
-        let mut arr2 = arr2;
-        arr2.sort_unstable();
-        let mut distance = 0;
-        for num1 in arr1 {
-            let insert_index = arr2.partition_point(|&num2| num2 < num1);
-            if insert_index < arr2.len() && arr2[insert_index] - num1 <= d {
-                continue;
-            }
-            if insert_index > 0 && num1 - arr2[insert_index - 1] <= d {
-                continue;
-            }
-            distance += 1;
+    pub fn create_target_array(nums: Vec<i32>, index: Vec<i32>) -> Vec<i32> {
+        let mut result = Vec::with_capacity(nums.len());
+        for (num, index) in nums.into_iter().zip(index.into_iter()) {
+            result.insert(index as usize, num);
         }
-        distance
+        result
     }
 }
