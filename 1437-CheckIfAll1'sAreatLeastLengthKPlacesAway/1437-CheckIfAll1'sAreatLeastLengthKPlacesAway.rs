66f3da9ff5f2b087361c0c15969317e17094d86f
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-06-03 14:14:24 +0300

    File updated at 03.06.2025, 14:14:23

--- 1437-CheckIfAll1'sAreatLeastLengthKPlacesAway/1437-CheckIfAll1'sAreatLeastLengthKPlacesAway.rs
+++ 1437-CheckIfAll1'sAreatLeastLengthKPlacesAway/1437-CheckIfAll1'sAreatLeastLengthKPlacesAway.rs
@@ -1,10 +1,12 @@
-// Last updated: 03.06.2025, 14:09:09
+// Last updated: 03.06.2025, 14:14:23
 impl Solution {
-    pub fn num_identical_pairs(nums: Vec<i32>) -> i32 {
-        let mut count = [0; 101];
-        for num in nums {
-            count[num as usize] += 1;
+    pub fn num_water_bottles(num_bottles: i32, num_exchange: i32) -> i32 {
+        let mut num_bottles = num_bottles;
+        let mut sum = num_bottles;
+        while num_bottles >= num_exchange {
+            sum += num_bottles / num_exchange;
+            num_bottles = num_bottles / num_exchange + num_bottles % num_exchange;
         }
-        count.into_iter().filter_map(|count| if count > 1 { Some(count * (count - 1) / 2) } else { None }).sum()
+        sum
     }
 }
