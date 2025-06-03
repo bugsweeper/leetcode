731a007a19dae87837ee14778b7d83e2c88c9dfe
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-06-03 15:11:04 +0300

    File updated at 03.06.2025, 15:11:04

--- 1437-CheckIfAll1'sAreatLeastLengthKPlacesAway/1437-CheckIfAll1'sAreatLeastLengthKPlacesAway.rs
+++ 1437-CheckIfAll1'sAreatLeastLengthKPlacesAway/1437-CheckIfAll1'sAreatLeastLengthKPlacesAway.rs
@@ -1,16 +1,20 @@
-// Last updated: 03.06.2025, 14:57:26
+// Last updated: 03.06.2025, 15:11:04
+const upper2lower: u8 = b'a' - b'A';
+
 impl Solution {
-    pub fn find_kth_positive(arr: Vec<i32>, k: i32) -> i32 {
-        let mut base = 0;
-        let mut size = arr.len();
-        while size > 1 {
-            let half = size >> 1;
-            let middle = base + half;
-            if arr[middle] <= middle as i32 + k {
-                base = middle;
+    pub fn make_good(s: String) -> String {
+        let mut stack = Vec::with_capacity(s.len());
+        for &byte in s.as_bytes() {
+            if let Some(&prev_byte) = stack.last() {
+                if prev_byte == byte + upper2lower || prev_byte + upper2lower == byte {
+                    stack.pop();
+                } else {
+                    stack.push(byte);
+                }
+            } else {
+                stack.push(byte);
             }
-            size -= half;
         }
-        k + base as i32 + if arr[base] <= base as i32 + k { 1 } else { 0 }
+        String::from_utf8(stack).unwrap()
     }
 }
