e30b542e539942cf8cfd7ffe85fa8915526caaf9
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-06-03 12:47:55 +0300

    File updated at 03.06.2025, 12:47:55

--- 1437-CheckIfAll1'sAreatLeastLengthKPlacesAway/1437-CheckIfAll1'sAreatLeastLengthKPlacesAway.rs
+++ 1437-CheckIfAll1'sAreatLeastLengthKPlacesAway/1437-CheckIfAll1'sAreatLeastLengthKPlacesAway.rs
@@ -1,9 +1,12 @@
-// Last updated: 03.06.2025, 12:42:23
+// Last updated: 03.06.2025, 12:47:55
 impl Solution {
-    pub fn max_product(nums: Vec<i32>) -> i32 {
-        let mut nums = nums;
-        let (max_index, max) = nums.iter().copied().enumerate().max_by_key(|&(index, value)| value).unwrap();
-        let max2 = nums.iter().copied().enumerate().filter_map(|(index, value)| if index == max_index {None} else {Some(value)}).max().unwrap();
-        (max - 1) * (max2 - 1)
+    pub fn shuffle(nums: Vec<i32>, n: i32) -> Vec<i32> {
+        let mut result = Vec::with_capacity(nums.len());
+        let middle = nums.len() / 2;
+        for (&x, &y) in nums[..middle].iter().zip(&nums[middle..]) {
+            result.push(x);
+            result.push(y);
+        }
+        result
     }
 }
