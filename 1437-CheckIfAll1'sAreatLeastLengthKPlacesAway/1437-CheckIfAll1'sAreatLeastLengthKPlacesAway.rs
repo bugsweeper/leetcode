11c61c7ddda0db5020f2b92445ff406129e6f808
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-06-03 12:42:23 +0300

    File updated at 03.06.2025, 12:42:23

--- 1437-CheckIfAll1'sAreatLeastLengthKPlacesAway/1437-CheckIfAll1'sAreatLeastLengthKPlacesAway.rs
+++ 1437-CheckIfAll1'sAreatLeastLengthKPlacesAway/1437-CheckIfAll1'sAreatLeastLengthKPlacesAway.rs
@@ -1,17 +1,9 @@
-// Last updated: 03.06.2025, 12:36:15
+// Last updated: 03.06.2025, 12:42:23
 impl Solution {
-    pub fn can_be_equal(target: Vec<i32>, arr: Vec<i32>) -> bool {
-        let mut count = [0; 1001];
-        for num in arr {
-            count[num as usize] += 1;
-        }
-        for num in target {
-            let count = count.get_mut(num as usize).unwrap();
-            if *count == 0 {
-                return false;
-            }
-            *count -= 1;
-        }
-        true
+    pub fn max_product(nums: Vec<i32>) -> i32 {
+        let mut nums = nums;
+        let (max_index, max) = nums.iter().copied().enumerate().max_by_key(|&(index, value)| value).unwrap();
+        let max2 = nums.iter().copied().enumerate().filter_map(|(index, value)| if index == max_index {None} else {Some(value)}).max().unwrap();
+        (max - 1) * (max2 - 1)
     }
 }
