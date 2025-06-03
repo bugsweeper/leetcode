ff4a8f8c44fe923efb70ac29e2d1b2367f795e79
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-06-03 13:48:00 +0300

    File updated at 03.06.2025, 13:47:59

--- 1437-CheckIfAll1'sAreatLeastLengthKPlacesAway/1437-CheckIfAll1'sAreatLeastLengthKPlacesAway.rs
+++ 1437-CheckIfAll1'sAreatLeastLengthKPlacesAway/1437-CheckIfAll1'sAreatLeastLengthKPlacesAway.rs
@@ -1,10 +1,23 @@
-// Last updated: 03.06.2025, 13:34:46
+// Last updated: 03.06.2025, 13:47:59
+use std::collections::HashSet;
+
 impl Solution {
-    pub fn xor_operation(n: i32, start: i32) -> i32 {
-        let mut sum = 0;
-        for num in (start..start + (n << 1)).step_by(2) {
-            sum ^= num;
+    pub fn is_path_crossing(path: String) -> bool {
+        let mut point_set = HashSet::with_capacity(path.len());
+        let (mut x, mut y) = (0, 0);
+        point_set.insert((x, y));
+        for &direction in path.as_bytes() {
+            match direction {
+                b'N' => y += 1,
+                b'S' => y -= 1,
+                b'E' => x += 1,
+                b'W' => x -= 1,
+                _ => unimplemented!(),
+            }
+            if !point_set.insert((x, y)) {
+                return true;
+            }
         }
-        sum
+        false
     }
 }
