55ce043d5706afa88e545a4e9c878644a9d1dc2a
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-06-03 14:55:53 +0300

    File updated at 03.06.2025, 14:55:53

--- 1437-CheckIfAll1'sAreatLeastLengthKPlacesAway/1437-CheckIfAll1'sAreatLeastLengthKPlacesAway.rs
+++ 1437-CheckIfAll1'sAreatLeastLengthKPlacesAway/1437-CheckIfAll1'sAreatLeastLengthKPlacesAway.rs
@@ -1,10 +1,16 @@
-// Last updated: 03.06.2025, 14:18:40
+// Last updated: 03.06.2025, 14:55:53
 impl Solution {
-    pub fn restore_string(s: String, indices: Vec<i32>) -> String {
-        let mut result = vec![b' '; s.len()];
-        for (index, &byte) in indices.into_iter().zip(s.as_bytes()) {
-            result[index as usize] = byte;
+    pub fn find_kth_positive(arr: Vec<i32>, k: i32) -> i32 {
+        let mut base = 0;
+        let mut size = arr.len();
+        while size > 1 {
+            let half = size >> 1;
+            let middle = base + half;
+            if arr[middle] - k <= middle as i32 {
+                base = middle;
+            }
+            size -= half;
         }
-        String::from_utf8(result).unwrap()
+        k + base as i32 + if arr[base] - k <= base as i32 { 1 } else { 0 }
     }
 }
