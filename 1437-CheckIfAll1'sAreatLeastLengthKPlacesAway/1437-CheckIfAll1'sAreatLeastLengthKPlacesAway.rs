8e38f7a86d2bbe1fdfba3f5b55775b5a49a56f0b
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-06-04 12:59:14 +0300

    File updated at 04.06.2025, 12:59:14

--- 1437-CheckIfAll1'sAreatLeastLengthKPlacesAway/1437-CheckIfAll1'sAreatLeastLengthKPlacesAway.rs
+++ 1437-CheckIfAll1'sAreatLeastLengthKPlacesAway/1437-CheckIfAll1'sAreatLeastLengthKPlacesAway.rs
@@ -1,12 +1,27 @@
-// Last updated: 04.06.2025, 12:20:42
+// Last updated: 04.06.2025, 12:59:14
 impl Solution {
-    pub fn most_visited(n: i32, rounds: Vec<i32>) -> Vec<i32> {
-        let first = *rounds.first().unwrap();
-        let last = *rounds.last().unwrap();
-        if last >= first {
-            (first..=last).collect::<Vec<_>>()
-        } else {
-            (1..=last).chain(first..=n).collect::<Vec<_>>()
+    pub fn contains_pattern(arr: Vec<i32>, m: i32, k: i32) -> bool {
+        let m = m as usize;
+        let k = k as usize;
+        for shift in 0..m {
+            if arr.len() < shift + m * k {
+                break;
+            }
+            let mut iter = arr[shift..].chunks_exact(m);
+            let mut prev_slice = iter.next().unwrap();
+            let mut count = 1;
+            for slice in iter {
+                if slice == prev_slice {
+                    count += 1;
+                    if count == k {
+                        return true;
+                    }
+                } else {
+                    prev_slice = slice;
+                    count = 1;
+                }
+            }
         }
+        false
     }
 }
