b9c3901bf3bd39d191064d59a913161d533d44ef
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-06-10 11:41:32 +0300

    File updated at 10.06.2025, 11:41:32

--- 1608-SpecialArrayWithXElementsGreaterThanorEqualX/1608-SpecialArrayWithXElementsGreaterThanorEqualX.rs
+++ 1608-SpecialArrayWithXElementsGreaterThanorEqualX/1608-SpecialArrayWithXElementsGreaterThanorEqualX.rs
@@ -1,17 +1,18 @@
-// Last updated: 10.06.2025, 11:37:56
+// Last updated: 10.06.2025, 11:41:32
 impl Solution {
-    pub fn special_array(nums: Vec<i32>) -> i32 {
-        let mut freq = [0; 1001];
-        let mut remains = nums.len();
-        for num in nums {
-            freq[num as usize] += 1;
-        }
-        for (num, freq) in freq.into_iter().enumerate() {
-            if num == remains {
-                return num as i32
+    pub fn max_depth(s: String) -> i32 {
+        let mut max_depth = 0;
+        let mut depth = 0;
+        for &byte in s.as_bytes() {
+            match byte {
+                b'(' => {
+                    depth += 1;
+                    max_depth = max_depth.max(depth);
+                }
+                b')' => depth -= 1,
+                _ => {}
             }
-            remains -= freq;
         }
-        -1
+        max_depth
     }
 }
