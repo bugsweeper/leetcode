a0019059344af7d8e5a59b7daf4e99be9d0dc3de
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-06-10 11:49:31 +0300

    File updated at 10.06.2025, 11:49:31

--- 1608-SpecialArrayWithXElementsGreaterThanorEqualX/1608-SpecialArrayWithXElementsGreaterThanorEqualX.rs
+++ 1608-SpecialArrayWithXElementsGreaterThanorEqualX/1608-SpecialArrayWithXElementsGreaterThanorEqualX.rs
@@ -1,18 +1,9 @@
-// Last updated: 10.06.2025, 11:41:32
+// Last updated: 10.06.2025, 11:49:31
 impl Solution {
-    pub fn max_depth(s: String) -> i32 {
-        let mut max_depth = 0;
-        let mut depth = 0;
-        for &byte in s.as_bytes() {
-            match byte {
-                b'(' => {
-                    depth += 1;
-                    max_depth = max_depth.max(depth);
-                }
-                b')' => depth -= 1,
-                _ => {}
-            }
-        }
-        max_depth
+    pub fn trim_mean(arr: Vec<i32>) -> f64 {
+        let mut arr = arr;
+        arr.sort_unstable();
+        let cut_count = arr.len() / 20;
+        arr[cut_count..arr.len() - cut_count].iter().sum::<i32>() as f64 / (arr.len() - 2 * cut_count) as f64
     }
 }
