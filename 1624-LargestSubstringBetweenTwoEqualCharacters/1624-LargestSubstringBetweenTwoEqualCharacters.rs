ccfcccd1e4fdec72d7b281fb00e3aafc79208e49
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-06-12 15:51:29 +0300

    File updated at 12.06.2025, 15:51:28

--- 1624-LargestSubstringBetweenTwoEqualCharacters/1624-LargestSubstringBetweenTwoEqualCharacters.rs
+++ 1624-LargestSubstringBetweenTwoEqualCharacters/1624-LargestSubstringBetweenTwoEqualCharacters.rs
@@ -1,18 +1,23 @@
-// Last updated: 12.06.2025, 15:41:03
-const SIZE: usize = b'z' as usize + 1;
+// Last updated: 12.06.2025, 15:51:28
+use std::cmp::Ordering;
 
 impl Solution {
-    pub fn max_length_between_equal_characters(s: String) -> i32 {
-        let mut min_index = [None; SIZE];
-        let mut max_index = [0; SIZE];
-        for (index, byte) in s.bytes().enumerate() {
-            let character = byte as usize;
-            let index = index as i32;
-            min_index[character].get_or_insert(index);
-            max_index[character] = index;
+    pub fn slowest_key(release_times: Vec<i32>, keys_pressed: String) -> char {
+        let mut prev_time = 0;
+        let mut max_delta = 0;
+        let mut slowest_key = b'a';
+        for (time, key) in release_times.into_iter().zip(keys_pressed.bytes()) {
+            let delta = time - prev_time;
+            prev_time = time;
+            match delta.cmp(&max_delta) {
+                Ordering::Greater => {
+                    max_delta = delta;
+                    slowest_key = key;
+                }
+                Ordering::Equal => slowest_key = slowest_key.max(key),
+                _ => {}
+            }
         }
-        min_index.into_iter().zip(max_index).filter_map(|(min, max)| {
-            Some(max - min? - 1)
-        }).max().unwrap()
+        slowest_key as char
     }
 }
