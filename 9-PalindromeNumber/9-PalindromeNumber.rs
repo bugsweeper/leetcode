1f2d3298e4f4d3e5b98139d64c8f223f796b015b
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-06-16 15:36:35 +0300

    File updated at 16.06.2025, 15:36:35

--- 9-PalindromeNumber/9-PalindromeNumber.rs
+++ 9-PalindromeNumber/9-PalindromeNumber.rs
@@ -1,3 +1,4 @@
+// Last updated: 16.06.2025, 15:36:35
 impl Solution {
     pub fn is_palindrome(x: i32) -> bool {
         let mut x = x;
@@ -8,15 +9,20 @@ impl Solution {
             return true;
         }
         let power_of_10 = x.ilog10();
-        let mut stack = (0..(power_of_10 + 1) / 2).map(|_| {
-            let digit = x % 10;
-            x /= 10;
-            digit
-        }).collect::<Vec<_>>();
+        // Put to stack right half of number
+        let mut stack = (0..power_of_10.div_ceil(2))
+            .map(|_| {
+                let digit = x % 10;
+                x /= 10;
+                digit
+            })
+            .collect::<Vec<_>>();
+        // Exclude middle digit for odd length number
         if power_of_10 % 2 == 0 {
             x /= 10;
         }
-        for _ in 0..(power_of_10 + 1) / 2 {
+        // Compare digits of left half with digits from stack
+        for _ in 0..power_of_10.div_ceil(2) {
             if x % 10 != stack.pop().unwrap() {
                 return false;
             }
