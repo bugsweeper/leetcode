42b48fd23f6199c03891e6c65f6a8b3e9a1bfd25
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-06-16 11:09:55 +0300

    File updated at 16.06.2025, 11:09:55

--- 1646-GetMaximuminGeneratedArray/1646-GetMaximuminGeneratedArray.rs
+++ 1646-GetMaximuminGeneratedArray/1646-GetMaximuminGeneratedArray.rs
@@ -1,34 +1,21 @@
-// Last updated: 16.06.2025, 11:00:10
-struct OrderedStream {
-    data: Vec<String>,
-    ptr: usize
-}
-
-
-/** 
- * `&self` means the method takes an immutable reference.
- * If you need a mutable reference, change it to `&mut self` instead.
- */
-impl OrderedStream {
-
-    fn new(n: i32) -> Self {
-        Self {
-            data: vec![String::new(); n as usize + 1],
-            ptr: 1,
+// Last updated: 16.06.2025, 11:09:55
+impl Solution {
+    pub fn array_strings_are_equal(word1: Vec<String>, word2: Vec<String>) -> bool {
+        let mut concatenated = String::with_capacity(1000);
+        for word in word1 {
+            concatenated.push_str(word.as_str());
         }
-    }
-    
-    fn insert(&mut self, id_key: i32, value: String) -> Vec<String> {
-        let id_key = id_key as usize;
-        self.data[id_key] = value;
-        let result = self.data[self.ptr..].iter().take_while(|value| value.len() == 5).cloned().collect::<Vec<_>>();
-        self.ptr += result.len();
-        result
+        let mut shift = 0;
+        for word in word2 {
+            if shift + word.len() > concatenated.len() {
+                return false;
+            }
+            let piece = &concatenated[shift..shift + word.len()];
+            if piece != &word {
+                return false;
+            }
+            shift += word.len();
+        }
+        shift == concatenated.len()
     }
 }
-
-/**
- * Your OrderedStream object will be instantiated and called as such:
- * let obj = OrderedStream::new(n);
- * let ret_1: Vec<String> = obj.insert(idKey, value);
- */
