8b36fcc0e6845063ccd9ff8780aec840252cbe0b
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-06-16 14:41:56 +0300

    File updated at 16.06.2025, 14:41:56

--- 1646-GetMaximuminGeneratedArray/1646-GetMaximuminGeneratedArray.rs
+++ 1646-GetMaximuminGeneratedArray/1646-GetMaximuminGeneratedArray.rs
@@ -1,22 +1,16 @@
-// Last updated: 16.06.2025, 14:32:31
+// Last updated: 16.06.2025, 14:41:56
 impl Solution {
-    pub fn maximum_units(box_types: Vec<Vec<i32>>, truck_size: i32) -> i32 {
-        let mut box_types = box_types
-            .into_iter().map(|box_type| (box_type[0], box_type[1]))
-            .collect::<Vec<_>>();
-        box_types.sort_unstable_by_key(|(_, units)| -units);
-        let mut remaining_boxes = truck_size;
-        let mut max_units = 0;
-        for (mut boxes, units) in box_types {
-            if boxes > remaining_boxes {
-                boxes = remaining_boxes;
+    pub fn total_money(n: i32) -> i32 {
+        let mut day_money = 6;
+        let mut total_money = 0;
+        for i in 0..n {
+            if i % 7 == 0 {
+                day_money -= 5;
+            } else {
+                day_money += 1;
             }
-            max_units += boxes * units;
-            remaining_boxes -= boxes;
-            if remaining_boxes == 0 {
-                break;
-            }
+            total_money += day_money;
         }
-        max_units
+        total_money
     }
 }
