0ccebe03832c0904831d7ac734a3c0b8e5c86fdb
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-06-16 15:07:09 +0300

    File updated at 16.06.2025, 15:07:08

--- 1646-GetMaximuminGeneratedArray/1646-GetMaximuminGeneratedArray.rs
+++ 1646-GetMaximuminGeneratedArray/1646-GetMaximuminGeneratedArray.rs
@@ -1,16 +1,13 @@
-// Last updated: 16.06.2025, 14:41:56
+// Last updated: 16.06.2025, 15:07:08
 impl Solution {
-    pub fn total_money(n: i32) -> i32 {
-        let mut day_money = 6;
-        let mut total_money = 0;
-        for i in 0..n {
-            if i % 7 == 0 {
-                day_money -= 5;
-            } else {
-                day_money += 1;
-            }
-            total_money += day_money;
+    pub fn decode(encoded: Vec<i32>, first: i32) -> Vec<i32> {
+        let mut prev_num = first;
+        let mut decoded = Vec::with_capacity(encoded.len() + 1);
+        decoded.push(prev_num);
+        for num in encoded {
+            prev_num ^= num;
+            decoded.push(prev_num);
         }
-        total_money
+        decoded
     }
 }
