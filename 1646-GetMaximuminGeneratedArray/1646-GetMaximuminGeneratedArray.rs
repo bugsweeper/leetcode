db3072220db3c84b5a15bd4cab38ce334f2b400d
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-06-16 11:00:10 +0300

    File updated at 16.06.2025, 11:00:10

--- 1646-GetMaximuminGeneratedArray/1646-GetMaximuminGeneratedArray.rs
+++ 1646-GetMaximuminGeneratedArray/1646-GetMaximuminGeneratedArray.rs
@@ -1,24 +1,34 @@
-// Last updated: 16.06.2025, 10:44:59
-impl Solution {
-    pub fn get_maximum_generated(n: i32) -> i32 {
-        if n < 2 {
-            return n;
+// Last updated: 16.06.2025, 11:00:10
+struct OrderedStream {
+    data: Vec<String>,
+    ptr: usize
+}
+
+
+/** 
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl OrderedStream {
+
+    fn new(n: i32) -> Self {
+        Self {
+            data: vec![String::new(); n as usize + 1],
+            ptr: 1,
         }
-        let n = n as usize;
-        let mut nums = Vec::with_capacity(n + 1);
-        nums.push(0);
-        nums.push(1);
-        let mut max = 1;
-        for i in 2..=n {
-            let num = if i & 1 == 0 {
-                nums[i >> 1]
-            } else {
-                let i = i >> 1;
-                nums[i] + nums[i + 1]
-            };
-            max = max.max(num);
-            nums.push(num);
-        }
-        max
+    }
+    
+    fn insert(&mut self, id_key: i32, value: String) -> Vec<String> {
+        let id_key = id_key as usize;
+        self.data[id_key] = value;
+        let result = self.data[self.ptr..].iter().take_while(|value| value.len() == 5).cloned().collect::<Vec<_>>();
+        self.ptr += result.len();
+        result
     }
 }
+
+/**
+ * Your OrderedStream object will be instantiated and called as such:
+ * let obj = OrderedStream::new(n);
+ * let ret_1: Vec<String> = obj.insert(idKey, value);
+ */
