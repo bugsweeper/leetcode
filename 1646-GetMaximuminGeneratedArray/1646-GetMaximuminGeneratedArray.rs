e13e293064740a20a30fdb76e2ed0c10ee684731
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-06-16 14:32:31 +0300

    File updated at 16.06.2025, 14:32:31

--- 1646-GetMaximuminGeneratedArray/1646-GetMaximuminGeneratedArray.rs
+++ 1646-GetMaximuminGeneratedArray/1646-GetMaximuminGeneratedArray.rs
@@ -1,27 +1,22 @@
-// Last updated: 16.06.2025, 14:08:36
-static IS_VOWEL: [bool; 123] = [
-    false, false, false, false, false, false, false, false, false, false, false, false, false,
-    false, false, false, false, false, false, false, false, false, false, false, false, false,
-    false, false, false, false, false, false, false, false, false, false, false, false, false,
-    false, false, false, false, false, false, false, false, false, false, false, false, false,
-    false, false, false, false, false, false, false, false, false, false, false, false, false,
-    true, false, false, false, true, false, false, false, true, false, false, false, false, false,
-    true, false, false, false, false, false, true, false, false, false, false, false, false, false,
-    false, false, false, false, true, false, false, false, true, false, false, false, true, false,
-    false, false, false, false, true, false, false, false, false, false, true, false, false, false,
-    false, false,
-];
-
+// Last updated: 16.06.2025, 14:32:31
 impl Solution {
-    pub fn halves_are_alike(s: String) -> bool {
-        let half = s.len() / 2;
-        s.bytes()
-            .take(half)
-            .filter(|byte| IS_VOWEL[*byte as usize])
-            .count()
-            == s.bytes()
-                .skip(half)
-                .filter(|byte| IS_VOWEL[*byte as usize])
-                .count()
+    pub fn maximum_units(box_types: Vec<Vec<i32>>, truck_size: i32) -> i32 {
+        let mut box_types = box_types
+            .into_iter().map(|box_type| (box_type[0], box_type[1]))
+            .collect::<Vec<_>>();
+        box_types.sort_unstable_by_key(|(_, units)| -units);
+        let mut remaining_boxes = truck_size;
+        let mut max_units = 0;
+        for (mut boxes, units) in box_types {
+            if boxes > remaining_boxes {
+                boxes = remaining_boxes;
+            }
+            max_units += boxes * units;
+            remaining_boxes -= boxes;
+            if remaining_boxes == 0 {
+                break;
+            }
+        }
+        max_units
     }
 }
