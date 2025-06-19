8f473f4b7ca97b9e1e62dc280d30b525e21c44ac
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-06-19 14:33:32 +0300

    File updated at 19.06.2025, 14:33:32

--- 1763-LongestNiceSubstring/1763-LongestNiceSubstring.rs
+++ 1763-LongestNiceSubstring/1763-LongestNiceSubstring.rs
@@ -1,23 +1,13 @@
-// Last updated: 19.06.2025, 14:28:58
+// Last updated: 19.06.2025, 14:33:32
 impl Solution {
-    pub fn nearest_valid_point(x: i32, y: i32, points: Vec<Vec<i32>>) -> i32 {
-        points
-            .into_iter()
-            .enumerate()
-            .fold((i32::MAX, -1), |(min_distance, min_index), (index, point)| {
-                let (a, b) = (point[0], point[1]);
-                if a == x {
-                    let distance = (b - y).abs();
-                    if min_distance > distance {
-                        return (distance, index as i32);
-                    }
-                } else if b == y {
-                    let distance = (a - x).abs();
-                    if min_distance > distance {
-                        return (distance, index as i32);
-                    }
-                }
-                (min_distance, min_index)
-            }).1
+    pub fn check_ones_segment(s: String) -> bool {
+        let mut prev = b'1';
+        for byte in s.bytes() {
+            if byte == b'1' && prev == b'0' {
+                return false;
+            }
+            prev = byte;
+        }
+        true
     }
 }
