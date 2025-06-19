c79b935258851a61843ecd23ce3abcdb27324f4c
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-06-19 14:47:33 +0300

    File updated at 19.06.2025, 14:47:33

--- 1763-LongestNiceSubstring/1763-LongestNiceSubstring.rs
+++ 1763-LongestNiceSubstring/1763-LongestNiceSubstring.rs
@@ -1,14 +1,25 @@
-// Last updated: 19.06.2025, 14:39:43
+// Last updated: 19.06.2025, 14:47:33
 impl Solution {
-    pub fn find_center(edges: Vec<Vec<i32>>) -> i32 {
-        let [edge0, edge1, ..] = &edges[..] else {
-            unimplemented!();
-        };
-        let (u0, v0, u1, v1) = (edge0[0], edge0[1], edge1[0], edge1[1]);
-        if u0 == u1 || u0 == v1 {
-            u0
-        } else {
-            v0
+    pub fn second_highest(s: String) -> i32 {
+        let mut digits = [false; 10];
+        for byte in s.bytes() {
+            if byte.is_ascii_digit() {
+                let digit = (byte - b'0') as usize;
+                match digit {
+                    8 => if digits[9] { return 8 },
+                    9 => if digits[8] { return 8 },
+                    _ => {}
+                }
+                digits[digit] = true;
+            }
         }
+        digits
+            .into_iter()
+            .enumerate()
+            .rev()
+            .filter_map(|(index, seen)| if seen { Some(index as i32) } else { None })
+            .skip(1)
+            .next()
+            .unwrap_or(-1)
     }
 }
