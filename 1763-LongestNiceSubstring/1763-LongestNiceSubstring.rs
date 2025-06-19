0c7ad8c2e5a6c5b52f4419b03dd1862b14534fd0
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-06-19 14:28:58 +0300

    File updated at 19.06.2025, 14:28:58

--- 1763-LongestNiceSubstring/1763-LongestNiceSubstring.rs
+++ 1763-LongestNiceSubstring/1763-LongestNiceSubstring.rs
@@ -1,15 +1,23 @@
-// Last updated: 19.06.2025, 14:05:07
+// Last updated: 19.06.2025, 14:28:58
 impl Solution {
-    pub fn count_matches(items: Vec<Vec<String>>, rule_key: String, rule_value: String) -> i32 {
-        let rule_index = match rule_key.as_bytes()[0] {
-            b't' => 0,
-            b'c' => 1,
-            b'n' => 2,
-            _ => unimplemented!(),
-        };
-        items
+    pub fn nearest_valid_point(x: i32, y: i32, points: Vec<Vec<i32>>) -> i32 {
+        points
             .into_iter()
-            .filter(|item| item[rule_index] == rule_value)
-            .count() as i32
+            .enumerate()
+            .fold((i32::MAX, -1), |(min_distance, min_index), (index, point)| {
+                let (a, b) = (point[0], point[1]);
+                if a == x {
+                    let distance = (b - y).abs();
+                    if min_distance > distance {
+                        return (distance, index as i32);
+                    }
+                } else if b == y {
+                    let distance = (a - x).abs();
+                    if min_distance > distance {
+                        return (distance, index as i32);
+                    }
+                }
+                (min_distance, min_index)
+            }).1
     }
 }
