e14cc3a38a7ca7e1305c8865e305a7f82ad3e458
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-06-19 15:43:41 +0300

    File updated at 19.06.2025, 15:43:40

--- 1763-LongestNiceSubstring/1763-LongestNiceSubstring.rs
+++ 1763-LongestNiceSubstring/1763-LongestNiceSubstring.rs
@@ -1,15 +1,16 @@
-// Last updated: 19.06.2025, 15:40:19
+// Last updated: 19.06.2025, 15:43:40
 impl Solution {
-    pub fn truncate_sentence(s: String, k: i32) -> String {
-        if let Some(index) = s
-            .bytes()
-            .enumerate()
-            .filter_map(|(index, byte)| if byte == b' ' { Some(index) } else { None })
-            .nth(k as usize - 1)
-        {
-            s[..index].into()
-        } else {
-            s
+    pub fn min_operations(nums: Vec<i32>) -> i32 {
+        let mut operations = 0;
+        let mut expected = 1;
+        for num in nums {
+            if num < expected {
+                operations += expected - num;
+                expected += 1;
+            } else {
+                expected = num + 1;
+            }
         }
+        operations
     }
 }
