4705bffea7dee28b932404879ff68e1ed97ce898
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-07-01 13:33:45 +0300

    File updated at 01.07.2025, 13:33:45

--- 1967-NumberofStringsThatAppearasSubstringsinWord/1967-NumberofStringsThatAppearasSubstringsinWord.rs
+++ 1967-NumberofStringsThatAppearasSubstringsinWord/1967-NumberofStringsThatAppearasSubstringsinWord.rs
@@ -1,9 +1,30 @@
-// Last updated: 01.07.2025, 11:17:56
+// Last updated: 01.07.2025, 13:33:45
 impl Solution {
-    pub fn num_of_strings(patterns: Vec<String>, word: String) -> i32 {
-        patterns
-            .into_iter()
-            .filter(|pattern| word.contains(pattern))
-            .count() as i32
+    pub fn valid_path(n: i32, edges: Vec<Vec<i32>>, source: i32, destination: i32) -> bool {
+        let (source, destination, n) = (source as usize, destination as usize, n as usize);
+        let mut connected = vec![vec![]; n];
+        for edge in edges {
+            let (u, v) = (edge[0] as usize, edge[1] as usize);
+            connected[u].push(v);
+            connected[v].push(u);
+        }
+        let mut seen = vec![false; n];
+        let mut stack = Vec::with_capacity(n);
+        seen[source] = true;
+        stack.push(source);
+        while let Some(vertex) = stack.pop() {
+            if vertex == destination {
+                return true;
+            }
+            for &neighbor in &connected[vertex] {
+                let seen = &mut seen[neighbor];
+                if *seen {
+                    continue;
+                }
+                *seen = true;
+                stack.push(neighbor);
+            }
+        }
+        false
     }
 }
