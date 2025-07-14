20330e41a662b33f7feb5755911e21e1efc81470
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-07-14 09:05:12 +0300

    Create 1290-ConvertBinaryNumberinaLinkedListtoInteger

---
+++ 1290-ConvertBinaryNumberinaLinkedListtoInteger/1290-ConvertBinaryNumberinaLinkedListtoInteger.rs
@@ -0,0 +1,29 @@
+// Last updated: 14.07.2025, 09:05:12
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn get_decimal_value(head: Option<Box<ListNode>>) -> i32 {
+        let mut node = &head;
+        let mut value = 0;
+        while node.is_some() {
+            let node_ref = node.as_ref().unwrap();
+            value = (value << 1) + node_ref.val;
+            node = &node_ref.next;
+        }
+        value
+    }
+}
