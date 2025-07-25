e567adab447ac65973ceecba63ba51c9ab191358
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-07-25 14:36:03 +0300

    File updated at 25.07.2025, 14:36:03

--- 2047-NumberofValidWordsinaSentence/2047-NumberofValidWordsinaSentence.rs
+++ 2047-NumberofValidWordsinaSentence/2047-NumberofValidWordsinaSentence.rs
@@ -1,11 +1,49 @@
-// Last updated: 25.07.2025, 13:40:42
+// Last updated: 25.07.2025, 14:36:03
+const ABC_LEN: usize = (b'z' - b'a' + 1) as usize;
+const IS_VOWEL: [bool; ABC_LEN] = [
+    true, false, false, false, true, false, false, false, true, false, false, false, false, false,
+    true, false, false, false, false, false, true, false, false, false, false, false,
+];
+
 impl Solution {
-    pub fn smallest_equal(nums: Vec<i32>) -> i32 {
-        for (index, num) in nums.into_iter().enumerate() {
-            if index % 10 == num as usize {
-                return index as i32;
+    pub fn count_vowel_substrings(word: String) -> i32 {
+        let (mut left_start, mut left_stop, mut vowels) = (0, 0, 0);
+        let mut vowel_count = [0; ABC_LEN];
+        let vowel_indexes = [b'a', b'e', b'i', b'o', b'u'].into_iter()
+            .map(|vowel| (vowel - b'a') as usize)
+            .collect::<Vec<_>>();
+        let mut substrings = 0;
+        for (right_index, byte) in word.bytes().enumerate() {
+            let right_byte_index = (byte - b'a') as usize;
+            if IS_VOWEL[right_byte_index] {
+                let count = &mut vowel_count[right_byte_index];
+                *count += 1;
+                if *count == 1 {
+                    vowels += 1;
+                }
+                if vowels < vowel_indexes.len() {
+                    continue;
+                }
+                for (index, byte) in word.bytes().enumerate().skip(left_stop) {
+                    let left_byte_index = (byte - b'a') as usize;
+                    let count = &mut vowel_count[left_byte_index];
+                    if *count > 1 {
+                        *count -= 1;
+                    } else {
+                        left_stop = index;
+                        break;
+                    }
+                }
+                substrings += left_stop - left_start + 1;
+            } else {
+                vowels = 0;
+                for vowel_index in &vowel_indexes {
+                    vowel_count[*vowel_index] = 0;
+                }
+                left_start = right_index + 1;
+                left_stop = left_start;
             }
         }
-        -1
+        substrings as i32
     }
 }
