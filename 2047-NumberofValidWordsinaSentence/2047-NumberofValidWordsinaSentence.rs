3973315cd8bf8cf19852fdd6f79b823ec79fcdc0
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-07-25 13:37:57 +0300

    File updated at 25.07.2025, 13:37:57

--- 2047-NumberofValidWordsinaSentence/2047-NumberofValidWordsinaSentence.rs
+++ 2047-NumberofValidWordsinaSentence/2047-NumberofValidWordsinaSentence.rs
@@ -1,36 +1,19 @@
-// Last updated: 25.07.2025, 13:23:30
+// Last updated: 25.07.2025, 13:37:57
+use std::collections::HashSet;
+
 impl Solution {
-    pub fn count_valid_words(sentence: String) -> i32 {
-        let mut words = 0;
-        'word: for word in sentence.split_ascii_whitespace() {
-            let mut prev_byte = b' ';
-            let mut seen_hyphen = false;
-            if word.is_empty() {
-                continue;
+    pub fn kth_distinct(arr: Vec<String>, k: i32) -> String {
+        let mut seen = HashSet::with_capacity(arr.len());
+        let mut repeated = HashSet::with_capacity(arr.len());
+        for item in &arr {
+            if !seen.insert(item) {
+                repeated.insert(item);
             }
-            for byte in word.bytes() {
-                if byte.is_ascii_digit() {
-                    continue 'word;
-                }
-                if prev_byte == b'-' && !byte.is_ascii_lowercase() {
-                    continue 'word;
-                }
-                if byte == b'-' {
-                    if seen_hyphen || !prev_byte.is_ascii_lowercase() {
-                        continue 'word;
-                    }
-                    seen_hyphen = true;
-                }
-                if [b'.', b',', b'!'].contains(&prev_byte) {
-                    continue 'word;
-                }
-                prev_byte = byte;
-            }
-            if prev_byte == b'-' {
-                continue;
-            }
-            words += 1;
         }
-        words
+        arr.iter()
+            .filter(|item| !repeated.contains(item))
+            .nth(k as usize - 1)
+            .cloned()
+            .unwrap_or(String::new())
     }
 }
