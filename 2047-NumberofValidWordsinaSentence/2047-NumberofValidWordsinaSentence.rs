f8e6bafbf5c8e32800f269313faf7abf070ca1be
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-07-25 14:48:20 +0300

    File updated at 25.07.2025, 14:48:20

--- 2047-NumberofValidWordsinaSentence/2047-NumberofValidWordsinaSentence.rs
+++ 2047-NumberofValidWordsinaSentence/2047-NumberofValidWordsinaSentence.rs
@@ -1,49 +1,22 @@
-// Last updated: 25.07.2025, 14:36:03
+// Last updated: 25.07.2025, 14:48:20
 const ABC_LEN: usize = (b'z' - b'a' + 1) as usize;
-const IS_VOWEL: [bool; ABC_LEN] = [
-    true, false, false, false, true, false, false, false, true, false, false, false, false, false,
-    true, false, false, false, false, false, true, false, false, false, false, false,
-];
+
+fn frequency(word: String) -> [u8; ABC_LEN] {
+    let mut freq = [0; ABC_LEN];
+    for byte in word.bytes() {
+        freq[(byte - b'a') as usize] += 1;
+    }
+    freq
+}
 
 impl Solution {
-    pub fn count_vowel_substrings(word: String) -> i32 {
-        let (mut left_start, mut left_stop, mut vowels) = (0, 0, 0);
-        let mut vowel_count = [0; ABC_LEN];
-        let vowel_indexes = [b'a', b'e', b'i', b'o', b'u'].into_iter()
-            .map(|vowel| (vowel - b'a') as usize)
-            .collect::<Vec<_>>();
-        let mut substrings = 0;
-        for (right_index, byte) in word.bytes().enumerate() {
-            let right_byte_index = (byte - b'a') as usize;
-            if IS_VOWEL[right_byte_index] {
-                let count = &mut vowel_count[right_byte_index];
-                *count += 1;
-                if *count == 1 {
-                    vowels += 1;
-                }
-                if vowels < vowel_indexes.len() {
-                    continue;
-                }
-                for (index, byte) in word.bytes().enumerate().skip(left_stop) {
-                    let left_byte_index = (byte - b'a') as usize;
-                    let count = &mut vowel_count[left_byte_index];
-                    if *count > 1 {
-                        *count -= 1;
-                    } else {
-                        left_stop = index;
-                        break;
-                    }
-                }
-                substrings += left_stop - left_start + 1;
-            } else {
-                vowels = 0;
-                for vowel_index in &vowel_indexes {
-                    vowel_count[*vowel_index] = 0;
-                }
-                left_start = right_index + 1;
-                left_stop = left_start;
-            }
+    pub fn check_almost_equivalent(word1: String, word2: String) -> bool {
+        if word1.len() <= 3 {
+            return true;
         }
-        substrings as i32
+        frequency(word1)
+            .into_iter()
+            .zip(frequency(word2))
+            .all(|(freq1, freq2)| freq1.abs_diff(freq2) <= 3)
     }
 }
