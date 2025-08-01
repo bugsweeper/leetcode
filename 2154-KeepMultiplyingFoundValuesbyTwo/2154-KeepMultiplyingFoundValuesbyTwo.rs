cbf951bd809f28b4b5dcdaa01632c31e84a5f910
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-08-01 11:23:33 +0300

    File updated at 01.08.2025, 11:23:32

--- 2154-KeepMultiplyingFoundValuesbyTwo/2154-KeepMultiplyingFoundValuesbyTwo.rs
+++ 2154-KeepMultiplyingFoundValuesbyTwo/2154-KeepMultiplyingFoundValuesbyTwo.rs
@@ -1,13 +1,17 @@
-// Last updated: 01.08.2025, 11:15:43
+// Last updated: 01.08.2025, 11:23:32
 impl Solution {
-    pub fn minimum_sum(num: i32) -> i32 {
-        let mut digits = Vec::with_capacity(4);
-        let mut num = num;
-        while num > 0 {
-            digits.push(num % 10);
-            num /= 10;
+    pub fn sort_even_odd(nums: Vec<i32>) -> Vec<i32> {
+        let mut nums = nums;
+        let mut odds = nums.iter().copied().skip(1).step_by(2).collect::<Vec<_>>();
+        odds.sort_unstable_by_key(|num| -num);
+        for (source, destination) in odds.into_iter().zip(nums.iter_mut().skip(1).step_by(2)) {
+            *destination = source;
         }
-        digits.sort_unstable();
-        (digits[0] + digits[1]) * 10 + digits[2] + digits[3]
+        let mut evens = nums.iter().copied().step_by(2).collect::<Vec<_>>();
+        evens.sort_unstable();
+        for (source, destination) in evens.into_iter().zip(nums.iter_mut().step_by(2)) {
+            *destination = source;
+        }
+        nums
     }
 }
