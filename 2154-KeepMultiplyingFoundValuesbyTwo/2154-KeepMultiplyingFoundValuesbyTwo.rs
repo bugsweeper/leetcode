fe466fa3fba1a5975f7acd792e4521c2ef00483d
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-08-01 11:43:01 +0300

    File updated at 01.08.2025, 11:43:01

--- 2154-KeepMultiplyingFoundValuesbyTwo/2154-KeepMultiplyingFoundValuesbyTwo.rs
+++ 2154-KeepMultiplyingFoundValuesbyTwo/2154-KeepMultiplyingFoundValuesbyTwo.rs
@@ -1,17 +1,12 @@
-// Last updated: 01.08.2025, 11:32:44
+// Last updated: 01.08.2025, 11:43:01
 impl Solution {
-    pub fn count_operations(num1: i32, num2: i32) -> i32 {
-        let mut count = 0;
-        let (mut num1, mut num2) = (num1, num2);
-        while num1 != 0 && num2 != 0 {
-            if num1 > num2 {
-                count += num1 / num2;
-                num1 %= num2;
-            } else {
-                count += num2 / num1;
-                num2 %= num1;
-            }
+    pub fn count_even(num: i32) -> i32 {
+        let mut digit_sum = 0;
+        let mut remaining = num;
+        while remaining != 0 {
+            digit_sum += remaining % 10;
+            remaining /= 10;
         }
-        count
+        (num - (digit_sum & 1)) >> 1
     }
 }
