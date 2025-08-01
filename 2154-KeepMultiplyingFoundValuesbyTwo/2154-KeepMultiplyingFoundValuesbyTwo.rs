280047cef8c61487b2efbffd52ed3626bbef27fd
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-08-01 12:10:16 +0300

    File updated at 01.08.2025, 12:10:16

--- 2154-KeepMultiplyingFoundValuesbyTwo/2154-KeepMultiplyingFoundValuesbyTwo.rs
+++ 2154-KeepMultiplyingFoundValuesbyTwo/2154-KeepMultiplyingFoundValuesbyTwo.rs
@@ -1,17 +1,10 @@
-// Last updated: 01.08.2025, 12:04:36
+// Last updated: 01.08.2025, 12:10:16
+use std::collections::HashSet;
+
 impl Solution {
-    pub fn cells_in_range(s: String) -> Vec<String> {
-        let s = s.as_bytes();
-        let (cs, ce, rs, re) = (s[0], s[3], s[1], s[4]);
-        let mut answer = Vec::with_capacity((ce - cs + 1) as usize * (re - rs + 1) as usize);
-        for col in s[0]..=s[3] {
-            for row in s[1]..=s[4] {
-                let mut cell = String::with_capacity(2);
-                cell.push(col as char);
-                cell.push(row as char);
-                answer.push(cell);
-            }
-        }
-        answer
+    pub fn find_difference(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<Vec<i32>> {
+        let mut nums1 = nums1.into_iter().collect::<HashSet<_>>();
+        let mut nums2 = nums2.into_iter().collect::<HashSet<_>>();
+        vec![nums1.difference(&nums2).copied().collect::<Vec<_>>(), nums2.difference(&nums1).copied().collect::<Vec<_>>()]
     }
 }
