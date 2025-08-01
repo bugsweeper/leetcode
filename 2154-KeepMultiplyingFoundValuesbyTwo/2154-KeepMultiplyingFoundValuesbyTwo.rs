eff0e82a2891e61e0b0f563b77720c37d4fba32a
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-08-01 11:52:38 +0300

    File updated at 01.08.2025, 11:52:38

--- 2154-KeepMultiplyingFoundValuesbyTwo/2154-KeepMultiplyingFoundValuesbyTwo.rs
+++ 2154-KeepMultiplyingFoundValuesbyTwo/2154-KeepMultiplyingFoundValuesbyTwo.rs
@@ -1,12 +1,23 @@
-// Last updated: 01.08.2025, 11:43:01
+// Last updated: 01.08.2025, 11:52:38
+use std::collections::HashMap;
+
 impl Solution {
-    pub fn count_even(num: i32) -> i32 {
-        let mut digit_sum = 0;
-        let mut remaining = num;
-        while remaining != 0 {
-            digit_sum += remaining % 10;
-            remaining /= 10;
+    pub fn most_frequent(nums: Vec<i32>, key: i32) -> i32 {
+        let mut count = HashMap::with_capacity(nums.len() >> 1);
+        let mut prev = 0;
+        for num in nums {
+            if prev == key {
+                count.entry(num).and_modify(|count| *count += 1).or_insert(1);
+            }
+            prev = num;
         }
-        (num - (digit_sum & 1)) >> 1
+        let (mut answer, mut max_count) = (0, 0);
+        for (num, count) in count {
+            if count > max_count {
+                answer = num;
+                max_count = count;
+            }
+        }
+        answer
     }
 }
