5afe504af7a9c06dc0a7650d8f8a3ffaa05a3229
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-08-01 09:35:59 +0300

    File updated at 01.08.2025, 09:35:59

--- 118-Pascal'sTriangle/118-Pascal'sTriangle.rs
+++ 118-Pascal'sTriangle/118-Pascal'sTriangle.rs
@@ -1,3 +1,4 @@
+// Last updated: 01.08.2025, 09:35:59
 impl Solution {
     pub fn generate(num_rows: i32) -> Vec<Vec<i32>> {
         let num_rows = num_rows as usize;
@@ -5,10 +6,10 @@ impl Solution {
         result.push(vec![1]);
         for i in 1..num_rows {
             let mut new_row = Vec::with_capacity(i + 1);
-            new_row.push(1);
-            let prev_row = result.get(i - 1).unwrap();
-            for j in 1..i {
-                new_row.push(prev_row[j-1] + prev_row[j]);
+            let mut prev_num1 = 0;
+            for &prev_num2 in result.get(i - 1).unwrap() {
+                new_row.push(prev_num1 + prev_num2);
+                prev_num1 = prev_num2;
             }
             new_row.push(1);
             result.push(new_row);
