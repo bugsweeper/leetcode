9b0ea758766d96478c50b0dba2dddc30ae78c927
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-08-01 12:12:37 +0300

    File updated at 01.08.2025, 12:12:37

--- 2154-KeepMultiplyingFoundValuesbyTwo/2154-KeepMultiplyingFoundValuesbyTwo.rs
+++ 2154-KeepMultiplyingFoundValuesbyTwo/2154-KeepMultiplyingFoundValuesbyTwo.rs
@@ -1,10 +1,6 @@
-// Last updated: 01.08.2025, 12:10:16
-use std::collections::HashSet;
-
+// Last updated: 01.08.2025, 12:12:37
 impl Solution {
-    pub fn find_difference(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<Vec<i32>> {
-        let mut nums1 = nums1.into_iter().collect::<HashSet<_>>();
-        let mut nums2 = nums2.into_iter().collect::<HashSet<_>>();
-        vec![nums1.difference(&nums2).copied().collect::<Vec<_>>(), nums2.difference(&nums1).copied().collect::<Vec<_>>()]
+    pub fn min_bit_flips(start: i32, goal: i32) -> i32 {
+        (start ^ goal).count_ones() as i32
     }
 }
