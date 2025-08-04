0f07415d5181a6c23c7da2f9be9c5fdf11c1b2cb
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-08-04 16:24:35 +0300

    File updated at 04.08.2025, 16:24:35

--- 2224-MinimumNumberofOperationstoConvertTime/2224-MinimumNumberofOperationstoConvertTime.rs
+++ 2224-MinimumNumberofOperationstoConvertTime/2224-MinimumNumberofOperationstoConvertTime.rs
@@ -1,27 +1,8 @@
-// Last updated: 04.08.2025, 14:39:40
-// Definition for a binary tree node.
-// #[derive(Debug, PartialEq, Eq)]
-// pub struct TreeNode {
-//   pub val: i32,
-//   pub left: Option<Rc<RefCell<TreeNode>>>,
-//   pub right: Option<Rc<RefCell<TreeNode>>>,
-// }
-// 
-// impl TreeNode {
-//   #[inline]
-//   pub fn new(val: i32) -> Self {
-//     TreeNode {
-//       val,
-//       left: None,
-//       right: None
-//     }
-//   }
-// }
-use std::rc::Rc;
-use std::cell::RefCell;
+// Last updated: 04.08.2025, 16:24:35
 impl Solution {
-    pub fn check_tree(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
-        let root_ref = root.as_ref().unwrap().borrow();
-        root_ref.val == root_ref.left.as_ref().unwrap().borrow().val + root_ref.right.as_ref().unwrap().borrow().val
+    pub fn find_closest_number(nums: Vec<i32>) -> i32 {
+        nums.into_iter()
+            .min_by_key(|&num| (num.abs() << 1) + if num > 0 { 0 } else { 1 })
+            .unwrap()
     }
 }
