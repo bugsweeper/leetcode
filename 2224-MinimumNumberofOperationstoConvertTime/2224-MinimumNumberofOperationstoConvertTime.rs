1efa91d079cdb971bf4fc02caee22073ebb35e3c
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-08-04 16:55:50 +0300

    File updated at 04.08.2025, 16:55:50

--- 2224-MinimumNumberofOperationstoConvertTime/2224-MinimumNumberofOperationstoConvertTime.rs
+++ 2224-MinimumNumberofOperationstoConvertTime/2224-MinimumNumberofOperationstoConvertTime.rs
@@ -1,35 +1,23 @@
-// Last updated: 04.08.2025, 16:41:35
+// Last updated: 04.08.2025, 16:55:50
+use std::collections::HashSet;
+
 impl Solution {
-    pub fn digit_sum(s: String, k: i32) -> String {
-        let mut round = s.into_bytes();
-        let k = k as usize;
-        for byte in &mut round {
-            *byte -= b'0';
+    pub fn intersection(nums: Vec<Vec<i32>>) -> Vec<i32> {
+        if nums.len() == 1 {
+            let mut nums = nums;
+            let mut nums = nums.pop().unwrap();
+            nums.sort_unstable();
+            return nums;
         }
-        while round.len() > k {
-            let mut next_round = Vec::with_capacity(round.len());
-            for group in round.chunks(k) {
-                let mut sum = 0;
-                for &byte in group {
-                    sum += byte as i32;
-                }
-                if sum >= 100 {
-                    next_round.push((sum / 100) as u8);
-                    sum %= 100;
-                    next_round.push((sum / 10) as u8);
-                    next_round.push((sum % 10) as u8);
-                } else if sum >= 10 {
-                    next_round.push((sum / 10) as u8);
-                    next_round.push((sum % 10) as u8);
-                } else {
-                    next_round.push(sum as u8);
-                }
-            }
-            std::mem::swap(&mut round, &mut next_round);
+        let iterations = nums.len() - 2;
+        let mut iter = nums.into_iter();
+        let mut set = iter.next().unwrap().into_iter().collect::<HashSet<i32>>();
+        for nums in iter {
+            let other_set = nums.into_iter().collect::<HashSet<_>>();
+            set = set.intersection(&other_set).copied().collect::<HashSet<_>>();
         }
-        round
-            .into_iter()
-            .map(|byte| (byte + b'0') as char)
-            .collect::<String>()
+        let mut nums = set.into_iter().collect::<Vec<_>>();
+        nums.sort_unstable();
+        nums
     }
 }
