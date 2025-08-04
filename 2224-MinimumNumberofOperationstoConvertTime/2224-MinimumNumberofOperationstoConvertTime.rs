1f00adedf09685ebd6de96f365c11fc0491411c6
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-08-04 16:41:35 +0300

    File updated at 04.08.2025, 16:41:35

--- 2224-MinimumNumberofOperationstoConvertTime/2224-MinimumNumberofOperationstoConvertTime.rs
+++ 2224-MinimumNumberofOperationstoConvertTime/2224-MinimumNumberofOperationstoConvertTime.rs
@@ -1,8 +1,35 @@
-// Last updated: 04.08.2025, 16:24:35
+// Last updated: 04.08.2025, 16:41:35
 impl Solution {
-    pub fn find_closest_number(nums: Vec<i32>) -> i32 {
-        nums.into_iter()
-            .min_by_key(|&num| (num.abs() << 1) + if num > 0 { 0 } else { 1 })
-            .unwrap()
+    pub fn digit_sum(s: String, k: i32) -> String {
+        let mut round = s.into_bytes();
+        let k = k as usize;
+        for byte in &mut round {
+            *byte -= b'0';
+        }
+        while round.len() > k {
+            let mut next_round = Vec::with_capacity(round.len());
+            for group in round.chunks(k) {
+                let mut sum = 0;
+                for &byte in group {
+                    sum += byte as i32;
+                }
+                if sum >= 100 {
+                    next_round.push((sum / 100) as u8);
+                    sum %= 100;
+                    next_round.push((sum / 10) as u8);
+                    next_round.push((sum % 10) as u8);
+                } else if sum >= 10 {
+                    next_round.push((sum / 10) as u8);
+                    next_round.push((sum % 10) as u8);
+                } else {
+                    next_round.push(sum as u8);
+                }
+            }
+            std::mem::swap(&mut round, &mut next_round);
+        }
+        round
+            .into_iter()
+            .map(|byte| (byte + b'0') as char)
+            .collect::<String>()
     }
 }
