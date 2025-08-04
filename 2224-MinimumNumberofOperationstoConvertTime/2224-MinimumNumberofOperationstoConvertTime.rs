0a9d3312e7b2240d0b4c6ec3df869cc6a143110e
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-08-04 14:09:34 +0300

    File updated at 04.08.2025, 14:09:34

--- 2224-MinimumNumberofOperationstoConvertTime/2224-MinimumNumberofOperationstoConvertTime.rs
+++ 2224-MinimumNumberofOperationstoConvertTime/2224-MinimumNumberofOperationstoConvertTime.rs
@@ -1,39 +1,6 @@
-// Last updated: 04.08.2025, 13:41:24
+// Last updated: 04.08.2025, 14:09:34
 impl Solution {
-    pub fn largest_integer(num: i32) -> i32 {
-        let (mut digits, mut odds, mut evens, mut num) = (
-            Vec::with_capacity(10),
-            Vec::with_capacity(10),
-            Vec::with_capacity(10),
-            num,
-        );
-        // Decompose to digits
-        while num > 0 {
-            let digit = num % 10;
-            num /= 10;
-            digits.push(digit);
-            if digit & 1 == 1 {
-                odds.push(digit);
-            } else {
-                evens.push(digit);
-            }
-        }
-        // Sort separately digits
-        odds.sort_unstable();
-        evens.sort_unstable();
-        // Combine sorted digits by parity
-        for digit in digits.iter_mut().rev() {
-            *digit = if *digit & 1 == 1 {
-                odds.pop()
-            } else {
-                evens.pop()
-            }
-            .unwrap();
-        }
-        // Recompose
-        for digit in digits.into_iter().rev() {
-            num = num * 10 + digit;
-        }
-        num
+    pub fn sum(num1: i32, num2: i32) -> i32 {
+        num1 + num2
     }
 }
