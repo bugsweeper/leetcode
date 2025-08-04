12e72e56c0c8668e3e7ce3716819f8e975709de8
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-08-04 14:39:40 +0300

    File updated at 04.08.2025, 14:39:40

--- 2224-MinimumNumberofOperationstoConvertTime/2224-MinimumNumberofOperationstoConvertTime.rs
+++ 2224-MinimumNumberofOperationstoConvertTime/2224-MinimumNumberofOperationstoConvertTime.rs
@@ -1,6 +1,27 @@
-// Last updated: 04.08.2025, 14:09:34
+// Last updated: 04.08.2025, 14:39:40
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
 impl Solution {
-    pub fn sum(num1: i32, num2: i32) -> i32 {
-        num1 + num2
+    pub fn check_tree(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        let root_ref = root.as_ref().unwrap().borrow();
+        root_ref.val == root_ref.left.as_ref().unwrap().borrow().val + root_ref.right.as_ref().unwrap().borrow().val
     }
 }
