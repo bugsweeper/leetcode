96f63204e8782444bedaf8e668286c9e325a76a7
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-08-05 15:17:33 +0300

    File updated at 05.08.2025, 15:17:33

--- 2248-IntersectionofMultipleArrays/2248-IntersectionofMultipleArrays.rs
+++ 2248-IntersectionofMultipleArrays/2248-IntersectionofMultipleArrays.rs
@@ -1,9 +1,25 @@
-// Last updated: 05.08.2025, 15:00:43
+// Last updated: 05.08.2025, 15:17:33
 impl Solution {
-    pub fn count_prefixes(words: Vec<String>, s: String) -> i32 {
-        words
-            .into_iter()
-            .filter(|word| s.starts_with(word))
-            .count() as i32
+    pub fn largest_good_integer(num: String) -> String {
+        if let Some(digit) = num
+            .as_bytes()
+            .windows(3)
+            .filter_map(|slice| {
+                let &[d1, d2, d3] = slice else {
+                    unimplemented!();
+                };
+                if d1 == d2 && d2 == d3 { Some(d1) } else { None }
+            })
+            .max()
+        {
+            let digit = digit as char;
+            let mut answer = String::with_capacity(3);
+            answer.push(digit);
+            answer.push(digit);
+            answer.push(digit);
+            answer
+        } else {
+            String::new()
+        }
     }
 }
