4fad8d86b07bfeea03d2f00f91be979684290d33
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-08-05 14:59:34 +0300

    File updated at 05.08.2025, 14:59:34

--- 2248-IntersectionofMultipleArrays/2248-IntersectionofMultipleArrays.rs
+++ 2248-IntersectionofMultipleArrays/2248-IntersectionofMultipleArrays.rs
@@ -1,23 +1,27 @@
-// Last updated: 05.08.2025, 12:20:59
+// Last updated: 05.08.2025, 14:59:34
 impl Solution {
-    pub fn intersection(nums: Vec<Vec<i32>>) -> Vec<i32> {
-        let should_be_seen_times = nums.len();
-        let mut seen_times = vec![0; 1001];
-        for nums in nums {
-            for num in nums {
-                seen_times[num as usize] += 1;
-            }
-        }
-        seen_times
-            .into_iter()
-            .enumerate()
-            .filter_map(|(num, seen_times)| {
-                if seen_times == should_be_seen_times {
-                    Some(num as i32)
-                } else {
-                    None
-                }
-            })
-            .collect::<Vec<_>>()
+    pub fn remove_digit(number: String, digit: char) -> String {
+        // Lets try find first occurrence of digit with greater digit following
+        let digit_byte = digit as u8;
+        let index = if digit != '9'
+            && let Some(index) = number
+                .as_bytes()
+                .windows(2)
+                .position(|slice| slice[0] == digit_byte && slice[1] > digit_byte)
+        {
+            index
+        } else {
+            // new number will be lower than current, remove closer from end
+            let place_from_end = number
+                .bytes()
+                .rev()
+                .position(|byte| byte == digit_byte)
+                .unwrap();
+            number.len() - place_from_end - 1
+        };
+        let mut answer = String::with_capacity(number.len() - 1);
+        answer.push_str(&number[..index]);
+        answer.push_str(&number[index + 1..]);
+        answer
     }
 }
