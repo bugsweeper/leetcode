85406d8e70118c16d49c38e2056318e4531b945f
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-08-09 21:14:38 +0300

    File updated at 09.08.2025, 21:14:38

--- 231-PowerofTwo/231-PowerofTwo.rs
+++ 231-PowerofTwo/231-PowerofTwo.rs
@@ -1,8 +1,22 @@
+// Last updated: 09.08.2025, 21:14:38
 impl Solution {
     pub fn is_power_of_two(n: i32) -> bool {
-        if n < 1 {
+        if n == 1{
+            return true;
+        }
+        if n<=0{
             return false;
         }
-        n.count_ones() == 1
+        for i in 0..5{
+            if 2_u32.pow(i as u32) == n as u32{
+                return true;
+            }
+        }
+        for i in 5..(n.isqrt()+1){
+            if 2_u32.pow(i as u32) == n as u32{
+                return true;
+            }
+        }
+        return false;
     }
 }
