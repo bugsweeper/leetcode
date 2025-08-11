015efdf32c0247299724d15bfe1139bbd8d271c1
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-08-11 13:43:09 +0300

    File updated at 11.08.2025, 13:43:09

--- 2283-CheckifNumberHasEqualDigitCountandDigitValue/2283-CheckifNumberHasEqualDigitCountandDigitValue.rs
+++ 2283-CheckifNumberHasEqualDigitCountandDigitValue/2283-CheckifNumberHasEqualDigitCountandDigitValue.rs
@@ -1,20 +1,29 @@
-// Last updated: 11.08.2025, 13:27:50
-fn min_max_game(nums: &[i32], bit: i32) -> i32 {
-    if nums.len() == 1 {
-        return nums[0]
-    }
-    let half = nums.len() >> 1;
-    let left = min_max_game(&nums[..half], 0);
-    let right = min_max_game(&nums[half..], 1);
-    if bit == 0 {
-        left.min(right)
-    } else {
-        left.max(right)
-    }
-}
-
+// Last updated: 11.08.2025, 13:43:09
 impl Solution {
-    pub fn min_max_game(nums: Vec<i32>) -> i32 {
-        min_max_game(&nums, 0)
+    pub fn strong_password_checker_ii(password: String) -> bool {
+        if password.len() < 8 {
+            return false;
+        }
+        let mut prev_byte = b' ';
+        let mut has_lowercase = false;
+        let mut has_uppercase = false;
+        let mut has_digit = false;
+        let mut has_special = false;
+        for byte in password.bytes() {
+            if byte == prev_byte {
+                return false;
+            }
+            if byte.is_ascii_digit() {
+                has_digit = true;
+            } else if byte.is_ascii_lowercase() {
+                has_lowercase = true;
+            } else if byte.is_ascii_uppercase() {
+                has_uppercase = true;
+            } else {
+                has_special = true;
+            }
+            prev_byte = byte;
+        }
+        has_digit && has_lowercase && has_uppercase && has_special
     }
 }
