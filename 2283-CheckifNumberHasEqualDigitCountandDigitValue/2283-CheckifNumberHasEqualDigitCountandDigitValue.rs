0f4b5a3758f39010c6d8b7a0eefb4d011ec1137f
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-08-11 13:19:46 +0300

    File updated at 11.08.2025, 13:19:46

--- 2283-CheckifNumberHasEqualDigitCountandDigitValue/2283-CheckifNumberHasEqualDigitCountandDigitValue.rs
+++ 2283-CheckifNumberHasEqualDigitCountandDigitValue/2283-CheckifNumberHasEqualDigitCountandDigitValue.rs
@@ -1,10 +1,22 @@
-// Last updated: 11.08.2025, 13:13:41
+// Last updated: 11.08.2025, 13:19:46
+const ABC_LEN: usize = (b'z' - b'a' + 1) as usize;
+
+#[inline]
+fn count(s: String) -> [i32; ABC_LEN] {
+    let mut count = [0; ABC_LEN];
+    for byte in s.bytes() {
+        count[(byte - b'a') as usize] += 1;
+    }
+    count
+}
+
 impl Solution {
-    pub fn digit_count(num: String) -> bool {
-        let mut count = [0; 10];
-        for byte in num.bytes() {
-            count[(byte - b'0') as usize] += 1;
-        }
-        num.bytes().enumerate().all(|(index, byte)| count[index] == byte - b'0')
+    pub fn rearrange_characters(s: String, target: String) -> i32 {
+        count(s)
+            .into_iter()
+            .zip(count(target))
+            .filter_map(|(available, target)| if target == 0 {None} else {Some(available / target)})
+            .min()
+            .unwrap()
     }
 }
