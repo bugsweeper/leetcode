4a560c7eb56846b37d0b15d64610018ececb7cd0
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-08-15 08:47:18 +0300

    File updated at 15.08.2025, 08:47:18

--- 342-PowerofFour/342-PowerofFour.rs
+++ 342-PowerofFour/342-PowerofFour.rs
@@ -1,13 +1,11 @@
+// Last updated: 15.08.2025, 08:47:18
 impl Solution {
     pub fn is_power_of_four(n: i32) -> bool {
         if n < 1 {
             return false;
         }
         let mut n = n;
-        while n > 3 {
-            if n & 3 != 0 {
-                return false;
-            }
+        while n & 3 == 0 {
             n >>= 2;
         }
         n == 1
