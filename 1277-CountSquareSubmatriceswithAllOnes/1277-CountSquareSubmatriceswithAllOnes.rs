c002b007a36a06aa799905a4ac5753a76de4fb83
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-08-20 16:23:49 +0300

    File updated at 20.08.2025, 16:23:49

--- 1277-CountSquareSubmatriceswithAllOnes/1277-CountSquareSubmatriceswithAllOnes.rs
+++ 1277-CountSquareSubmatriceswithAllOnes/1277-CountSquareSubmatriceswithAllOnes.rs
@@ -1,36 +1,24 @@
-// Last updated: 20.08.2025, 16:12:09
+// Last updated: 20.08.2025, 16:23:49
 impl Solution {
-    pub fn count_squares(matrix: Vec<Vec<i32>>) -> i32 {
-        let width = matrix[0].len();
-        let mut column_height = vec![0; width];
-        let mut prev_row_square_size = vec![0; width];
-        let mut square_size = vec![0; width];
-        let mut squares = 0;
-        for row in matrix {
-            let mut row_width = 0;
-            for (column, (cell, column_height)) in
-                row.into_iter().zip(&mut column_height).enumerate()
-            {
-                if cell == 1 {
-                    let cur_square_size = if column == 0 {
-                        1
+    pub fn count_squares(mut matrix: Vec<Vec<i32>>) -> i32 {
+        let n = matrix.len();
+        let m = matrix[0].len();
+        let mut res = 0;
+        for i in 0..n {
+            for j in 0..m {
+                if matrix[i][j] == 1 {
+                    matrix[i][j] = if i >0 && j > 0 {
+                        1 + [matrix[i - 1][j], matrix[i - 1][j - 1], matrix[i][j - 1]]
+                            .iter()
+                            .min()
+                            .unwrap()
                     } else {
-                        1 + row_width
-                            .min(*column_height)
-                            .min(prev_row_square_size[column - 1])
-                    };
-                    row_width += 1;
-                    *column_height += 1;
-                    square_size[column] = cur_square_size;
-                    squares += cur_square_size;
-                } else {
-                    row_width = 0;
-                    *column_height = 0;
-                    square_size[column] = 0;
+                        1
+                    }
                 }
+                res += matrix[i][j];
             }
-            std::mem::swap(&mut prev_row_square_size, &mut square_size);
         }
-        squares
+        res
     }
 }
