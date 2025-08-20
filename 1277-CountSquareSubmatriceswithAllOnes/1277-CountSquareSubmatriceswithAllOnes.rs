322222eeb7d93d0d0e87c05944f96c849b70cb3e
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-08-20 16:26:13 +0300

    File updated at 20.08.2025, 16:26:13

--- 1277-CountSquareSubmatriceswithAllOnes/1277-CountSquareSubmatriceswithAllOnes.rs
+++ 1277-CountSquareSubmatriceswithAllOnes/1277-CountSquareSubmatriceswithAllOnes.rs
@@ -1,24 +1,30 @@
-// Last updated: 20.08.2025, 16:23:49
+// Last updated: 20.08.2025, 16:26:13
 impl Solution {
-    pub fn count_squares(mut matrix: Vec<Vec<i32>>) -> i32 {
-        let n = matrix.len();
-        let m = matrix[0].len();
-        let mut res = 0;
-        for i in 0..n {
-            for j in 0..m {
-                if matrix[i][j] == 1 {
-                    matrix[i][j] = if i >0 && j > 0 {
-                        1 + [matrix[i - 1][j], matrix[i - 1][j - 1], matrix[i][j - 1]]
-                            .iter()
-                            .min()
-                            .unwrap()
-                    } else {
-                        1
-                    }
+    pub fn count_squares(matrix: Vec<Vec<i32>>) -> i32 {
+        let width = matrix[0].len();
+        let mut prev_row_square_size = vec![0; width];
+        let mut square_size = vec![0; width];
+        let mut squares = 0;
+        for row in matrix {
+            square_size[0] = if row[0] == 1 {
+                squares += 1;
+                1
+            } else {
+                0
+            };
+            for (column, cell) in row.into_iter().enumerate().skip(1) {
+                square_size[column] = if cell == 1 {
+                    let cur_square_size = 1 + square_size[column - 1]
+                        .min(prev_row_square_size[column])
+                        .min(prev_row_square_size[column - 1]);
+                    squares += cur_square_size;
+                    cur_square_size
+                } else {
+                    0
                 }
-                res += matrix[i][j];
             }
+            std::mem::swap(&mut prev_row_square_size, &mut square_size);
         }
-        res
+        squares
     }
 }
