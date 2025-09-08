53f1f10188984720a32048a6a14c60ef94b05cb5
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-09-08 16:40:35 +0300

    File updated at 08.09.2025, 16:40:35

--- 2331-EvaluateBooleanBinaryTree/2331-EvaluateBooleanBinaryTree.rs
+++ 2331-EvaluateBooleanBinaryTree/2331-EvaluateBooleanBinaryTree.rs
@@ -1,10 +1,22 @@
-// Last updated: 08.09.2025, 16:26:43
+// Last updated: 08.09.2025, 16:40:35
 impl Solution {
-    pub fn minimum_operations(nums: Vec<i32>) -> i32 {
-        let mut seen = vec![false; 101];
-        for num in nums {
-            seen[num as usize] = true;
+    pub fn merge_similar_items(items1: Vec<Vec<i32>>, items2: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        let mut weights = vec![0; 1001];
+        for items in [items1, items2] {
+            for item in items {
+                weights[item[0] as usize] += item[1];
+            }
         }
-        seen.into_iter().skip(1).filter(|&seen| seen).count() as i32
+        weights
+            .into_iter()
+            .enumerate()
+            .filter_map(|(index, weight)| {
+                if weight > 0 {
+                    Some(vec![index as i32, weight])
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<_>>()
     }
 }
