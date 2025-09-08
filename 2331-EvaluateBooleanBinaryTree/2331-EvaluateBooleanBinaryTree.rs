74e1a8cc955ce430df64df60f85c4a08b6b40147
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-09-08 16:22:30 +0300

    File updated at 08.09.2025, 16:22:29

--- 2331-EvaluateBooleanBinaryTree/2331-EvaluateBooleanBinaryTree.rs
+++ 2331-EvaluateBooleanBinaryTree/2331-EvaluateBooleanBinaryTree.rs
@@ -1,24 +1,14 @@
-// Last updated: 08.09.2025, 16:18:38
+// Last updated: 08.09.2025, 16:22:29
 impl Solution {
-    pub fn best_hand(ranks: Vec<i32>, suits: Vec<char>) -> String {
-        let first_suit = suits[0];
-        if suits.into_iter().skip(1).all(|suit| suit == first_suit) {
-            return "Flush".into();
-        }
-        let mut max_rank_count = 0;
-        let mut rank_count = vec![0; 14];
-        for rank in ranks {
-            let rank_count = &mut rank_count[rank as usize];
-            *rank_count += 1;
-            if *rank_count == 3 {
-                return "Three of a Kind".into();
+    pub fn repeated_character(s: String) -> char {
+        let mut seen = vec![false; b'z' as usize + 1];
+        for byte in s.bytes() {
+            let mut seen = &mut seen[byte as usize];
+            if *seen {
+                return byte as char;
             }
-            max_rank_count = max_rank_count.max(*rank_count);
+            *seen = true;
         }
-        return if max_rank_count == 2 {
-            "Pair"
-        } else {
-            "High Card"
-        }.into()
+        unimplemented!()
     }
 }
