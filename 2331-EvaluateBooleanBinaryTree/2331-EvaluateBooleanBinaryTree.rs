f956765962c73adc65ff0e9acab13dcd29214b9e
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-09-08 14:55:38 +0300

    File updated at 08.09.2025, 14:55:37

--- 2331-EvaluateBooleanBinaryTree/2331-EvaluateBooleanBinaryTree.rs
+++ 2331-EvaluateBooleanBinaryTree/2331-EvaluateBooleanBinaryTree.rs
@@ -1,14 +1,16 @@
-// Last updated: 08.09.2025, 14:50:11
+// Last updated: 08.09.2025, 14:55:37
 impl Solution {
-    pub fn fill_cups(amount: Vec<i32>) -> i32 {
-        let mut amount = amount;
-        amount.sort_unstable();
-        let [low, middle, high] = amount[..] else {
-            unimplemented!();
-        };
-        if high > low + middle {
-            return high
+    pub fn number_of_pairs(nums: Vec<i32>) -> Vec<i32> {
+        let mut count = vec![0; 101];
+        for &num in &nums {
+            count[num as usize] += 1;
         }
-        (low + middle + high + 1) >> 1
+        let mut leftover = 0;
+        for count in count {
+            if count & 1 == 1 {
+                leftover += 1;
+            }
+        }
+        vec![(nums.len() as i32 - leftover) >> 1, leftover]
     }
 }
