50f3009702fbc1a1a001e0c9dcdd1f76cfed38a2
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-09-08 14:50:12 +0300

    File updated at 08.09.2025, 14:50:11

--- 2331-EvaluateBooleanBinaryTree/2331-EvaluateBooleanBinaryTree.rs
+++ 2331-EvaluateBooleanBinaryTree/2331-EvaluateBooleanBinaryTree.rs
@@ -1,41 +1,14 @@
-// Last updated: 08.09.2025, 10:18:56
-// Definition for a binary tree node.
-// #[derive(Debug, PartialEq, Eq)]
-// pub struct TreeNode {
-//   pub val: i32,
-//   pub left: Option<Rc<RefCell<TreeNode>>>,
-//   pub right: Option<Rc<RefCell<TreeNode>>>,
-// }
-// 
-// impl TreeNode {
-//   #[inline]
-//   pub fn new(val: i32) -> Self {
-//     TreeNode {
-//       val,
-//       left: None,
-//       right: None
-//     }
-//   }
-// }
-use std::rc::Rc;
-use std::cell::RefCell;
-
-fn evaluate_tree(node: &Option<Rc<RefCell<TreeNode>>>) -> bool {
-    let node_ref = node.as_ref().unwrap().borrow();
-    match node_ref.val {
-        0 => return false,
-        1 => return true,
-        _ => {}
-    }
-    let left = evaluate_tree(&node_ref.left);
-    match (left, node_ref.val) {
-        (true, 2) | (false, 3) => return left,
-        _ => evaluate_tree(&node_ref.right),
-    }
-}
-
+// Last updated: 08.09.2025, 14:50:11
 impl Solution {
-    pub fn evaluate_tree(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
-        evaluate_tree(&root)
+    pub fn fill_cups(amount: Vec<i32>) -> i32 {
+        let mut amount = amount;
+        amount.sort_unstable();
+        let [low, middle, high] = amount[..] else {
+            unimplemented!();
+        };
+        if high > low + middle {
+            return high
+        }
+        (low + middle + high + 1) >> 1
     }
 }
