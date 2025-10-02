227bb0f059046862b6ab3e5fb21beb5509463f8d
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-10-02 07:36:34 +0300

    File updated at 02.10.2025, 07:36:34

--- 3100-WaterBottlesII/3100-WaterBottlesII.rs
+++ 3100-WaterBottlesII/3100-WaterBottlesII.rs
@@ -1,14 +1,13 @@
-// Last updated: 02.10.2025, 07:31:53
+// Last updated: 02.10.2025, 07:36:34
 impl Solution {
     pub fn max_bottles_drunk(num_bottles: i32, num_exchange: i32) -> i32 {
         let mut empty_bottles = num_bottles;
-        let mut bottles_drunk = num_bottles;
+        let diff = num_bottles - num_exchange;
         let mut num_exchange = num_exchange;
         while empty_bottles >= num_exchange {
-            bottles_drunk += 1;
             empty_bottles -= num_exchange - 1;
             num_exchange += 1;
         }
-        bottles_drunk
+        num_exchange + diff
     }
 }
