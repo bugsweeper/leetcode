9c7d47fa7677700fcfd3799e86953b8659e17072
Vitaliy Sapronenko <vsapronenko@gmail.com>
2025-10-13 16:20:13 +0300

    File updated at 13.10.2025, 16:20:13

--- 2273-FindResultantArrayAfterRemovingAnagrams/2273-FindResultantArrayAfterRemovingAnagrams.rs
+++ 2273-FindResultantArrayAfterRemovingAnagrams/2273-FindResultantArrayAfterRemovingAnagrams.rs
@@ -1,7 +1,23 @@
-// Last updated: 11.08.2025, 12:50:04
+// Last updated: 13.10.2025, 16:20:13
+const ABC_LEN: usize = (b'z' - b'a' + 1) as usize;
+
 impl Solution {
-    pub fn percentage_letter(s: String, letter: char) -> i32 {
-        let goal_byte = letter as u8;
-        (s.bytes().filter(|byte| goal_byte.eq(byte)).count() * 100 / s.len()) as i32
+    pub fn remove_anagrams(words: Vec<String>) -> Vec<String> {
+        let mut prev_count = [0; ABC_LEN];
+        words
+            .into_iter()
+            .filter(|word| {
+                let mut count = [0; ABC_LEN];
+                for byte in word.bytes() {
+                    count[(byte - b'a') as usize] += 1;
+                }
+                if count == prev_count {
+                    false
+                } else {
+                    prev_count = count;
+                    true
+                }
+            })
+            .collect()
     }
 }
